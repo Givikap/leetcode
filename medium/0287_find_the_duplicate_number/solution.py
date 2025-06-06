04f9e93ce7da8b83a675c0f652ba55ba809e6855
Givikap <kaitosekiya@outlook.com>
2025-06-05 23:48:35 -0500

    Add first solution for '287. Find the Duplicate Number' problem

---
+++ medium/0287_find_the_duplicate_number/solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        sorted_unique_nums = list(range(1, len(nums) + 1))
+
+        for num in nums:
+            if sorted_unique_nums[num - 1] == -1:
+                return num
+
+            sorted_unique_nums[num - 1] = -1
