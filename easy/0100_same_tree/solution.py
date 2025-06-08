70b9c1bd1f102c7119d3a7d06c17d2e55d5e08eb
Givikap <kaitosekiya@outlook.com>
2025-06-07 23:19:53 -0500

    Add second shorter solution for '100. Same tree' problem

--- easy/0100_same_tree/solution.py
+++ easy/0100_same_tree/solution.py
@@ -11,10 +11,7 @@ class Solution:
         if not p and not q:
             return True
 
-        if p and q:
-            if p.val == q.val:
-                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-            else:
-                return False
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         else:
             return False
