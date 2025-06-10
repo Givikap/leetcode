46381890e6bcd0c5b658d67b046146fad3070a74
Givikap <kaitosekiya@outlook.com>
2025-06-09 22:28:28 -0500

    Add first Python solution for '66. Plus One' problem

---
+++ easy/0066_plus_one/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in reversed(range(len(digits))):
+            digits[i] += 1
+
+            if digits[i] == 10:
+                digits[i] = 0
+
+                if i == 0:
+                    return [1] + digits
+                
+                continue
+            
+            return digits
