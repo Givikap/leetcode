b2dc6ac469425dd4aeb9482dc8d426ba118210fb
Givikap <kaitosekiya@outlook.com>
2025-06-10 21:48:10 -0500

    Add second shorter solution for 'Remove nth Node From End of List' problem

--- medium/0019_remove_nth_node_from_end_of_list/solution.py
+++ medium/0019_remove_nth_node_from_end_of_list/solution.py
@@ -10,29 +10,21 @@ class Solution:
         if not head.next:
             return None
 
-        if not head.next.next:
-            if n == 2:
-                head = head.next
-            else:
-                head.next = None
-
-            return head
-
-        fast, slow = head, head
-        length = 0
-
+        fast = head
+        
         for i in range(n):
             fast = fast.next
 
         if not fast:
             return head.next
 
+        fast = fast.next
+        slow = head
+
         while fast:
             fast = fast.next
-
-            if fast:
-                slow = slow.next
+            slow = slow.next
 
         slow.next = slow.next.next
-        
+    
         return head
