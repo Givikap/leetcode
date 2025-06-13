6362f99c51d37f029cfae2c891a0f4fc6d5bc653
Givikap <kaitosekiya@outlook.com>
2025-06-12 23:40:20 -0500

    Add third but now working solution for 'Best Time to Buy and Sell Stocks' problem

--- easy/0121_best_time_to_buy_and_sell_stocks/solution.py
+++ easy/0121_best_time_to_buy_and_sell_stocks/solution.py
@@ -2,20 +2,11 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
         max_profit = 0
-        i_buy = 0
 
-        while i_buy < len(prices) - 1:
-            while i_buy < len(prices) - 1 and prices[i_buy] >= prices[i_buy+1]:
-                i_buy += 1
-
-            i_sell = i_buy + 1
-            next_i_buy = i_sell + 1
-
-            for i_sell in range(i_buy + 1, len(prices)):
-                if prices[i_sell] - prices[i_buy] > max_profit:
-                    max_profit = prices[i_sell] - prices[i_buy]
-
-            i_buy = next_i_buy
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
 
         return max_profit
