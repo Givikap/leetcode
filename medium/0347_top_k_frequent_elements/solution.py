eb6d59deb6fa43410c9f44200156faab1e71172e
Givikap <kaitosekiya@outlook.com>
2025-06-13 23:19:16 -0500

    Add first solution for 'Top K Frequent Elements' problem

---
+++ medium/0347_top_k_frequent_elements/solution.py
@@ -0,0 +1,15 @@
+import heapq
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequencies_map = {}
+
+        for num in nums:
+            frequencies_map[num] = frequencies_map.get(num, 0) + 1
+
+        pq = [(-frequency, num) for num, frequency in frequencies_map.items()]
+
+        heapq.heapify(pq)
+
+        return [heapq.heappop(pq)[1] for _ in range(k)]
