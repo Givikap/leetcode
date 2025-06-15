cb22d8351515e79c2c949c1878ee9726c012a4a3
Givikap <kaitosekiya@outlook.com>
2025-06-15 04:43:38 -0500

    Add third solution with bitwise operations for '268. Missing Number' problem

--- easy/0268_missing_number/solution.py
+++ easy/0268_missing_number/solution.py
@@ -2,4 +2,12 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        return len(nums) * (len(nums) + 1) // 2 - sum(nums)
+        cumilative_xor = 0
+
+        for i in range(len(nums) + 1):
+            cumilative_xor ^= i
+
+        for num in nums:
+            cumilative_xor ^= num
+
+        return cumilative_xor
