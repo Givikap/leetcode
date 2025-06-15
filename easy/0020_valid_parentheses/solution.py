cd83ff773193ef33023a1274a9b332b8932caf6b
Givikap <kaitosekiya@outlook.com>
2025-06-14 23:06:07 -0500

    Add first Python solution for '20. Valid Parentheses' problem

---
+++ easy/0020_valid_parentheses/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets_map = {'(': ')', '{': '}', '[': ']'}
+        stack = []
+
+        for c in s:
+            if c in brackets_map:
+                stack.append(c)
+            elif len(stack) > 0 and brackets_map[stack[-1]] == c:
+                stack.pop()
+            else:
+                return False
+
+        return len(stack) == 0
