e6579a6c5cab534bf67dc211609b9dc20437f3bf
Givikap <kaitosekiya@outlook.com>
2025-06-26 22:10:17 -0500

    Add first Python solution for '695. Max Area of Island' problem

---
+++ medium/0695_max_area_of_island/solution.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        max_area = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    stack = {(r, c)}
+                    curr_area = 0
+            
+                    while stack:
+                        rr, cc = stack.pop()
+
+                        grid[rr][cc] = 0
+                        curr_area += 1
+
+                        if rr + 1 < rows and grid[rr+1][cc] == 1:
+                            stack.add((rr + 1, cc))
+                        if cc + 1 < cols and grid[rr][cc+1] == 1:
+                            stack.add((rr, cc + 1))
+                        if rr - 1 >= 0 and grid[rr-1][cc] == 1:
+                            stack.add((rr - 1, cc))
+                        if cc - 1 >= 0 and grid[rr][cc-1] == 1:
+                            stack.add((rr, cc - 1))
+
+                    if curr_area > max_area:
+                        max_area = curr_area
+
+        return max_area
