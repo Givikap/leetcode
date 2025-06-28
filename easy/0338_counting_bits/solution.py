84fe00a84464b667cbab6d5c3b78bcb0414f8dc6
Givikap <kaitosekiya@outlook.com>
2025-06-27 20:03:18 -0500

    Add first Python solution for '338. Counting Bits' problem

---
+++ easy/0338_counting_bits/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bits_counter_list = [0]
+        offset = 1
+
+        for i in range(1, n+1):
+            if offset * 2 == i:
+                offset = i
+
+            bits_counter_list.append(1 + bits_counter_list[i - offset])
+
+        return bits_counter_list
