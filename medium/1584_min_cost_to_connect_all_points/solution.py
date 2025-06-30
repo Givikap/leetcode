1b6a771470add78a378162f2bbfeca7c01591658
Givikap <kaitosekiya@outlook.com>
2025-06-29 23:34:24 -0500

    Add second faster Python solution for '1584. Min Cost to Connect All Points' problem

--- medium/1584_min_cost_to_connect_all_points/solution.py
+++ medium/1584_min_cost_to_connect_all_points/solution.py
@@ -10,27 +10,24 @@ class Solution:
         curr = 0
 
         while len(visited) != len(points):
-            visited.add(tuple(points[curr]))
+            visited.add(curr)
 
             for i, point in enumerate(points):
-                if points[curr] == point:
+                if curr == i or (curr in visited and i in visited):
                     continue
 
                 heapq.heappush(heap, (
-                    abs(points[curr][0] - point[0]) + abs(points[curr][-1] - point[1]),
-                    tuple(points[curr]),
-                    tuple(point),
-                    i
+                    abs(points[curr][0] - point[0]) + abs(points[curr][1] - point[1]),
+                    (curr, i)
                 ))
 
             while heap:
-                distance, p1, p2, i = heapq.heappop(heap)
+                distance, (i1, i2) = heapq.heappop(heap)
 
-                if not (p1 in visited and p2 in visited):
-                    visited.add(p2)
-
+                if not (i1 in visited and i2 in visited):
+                    visited.add(i2)
                     min_cost += distance
-                    curr = i
+                    curr = i2
                     break
 
         return min_cost 
