0c7dcb47c94062f5d42171a2247f89edb0546f02
Givikap <kaitosekiya@outlook.com>
2025-07-02 19:59:31 -0500

    Add first Python solution for '1046. Last Stone Weight' problem

---
+++ easy/1046_last_stone_weight/solution.py
@@ -0,0 +1,19 @@
+import heapq
+from typing import List
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        heap = [-stone for stone in stones]
+        heapq.heapify(heap)
+
+        while heap:
+            if len(heap) == 1:
+                return -heap[0]
+
+            x = -heapq.heappop(heap)
+            y = -heapq.heappop(heap)
+
+            if x != y:
+                heapq.heappush(heap, -(x-y))
+
+        return 0
