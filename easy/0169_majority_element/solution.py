ac6f60bbda9991d7c2b95e0235378f739cf5051b
Givikap <kaitosekiya@outlook.com>
2025-07-04 23:37:17 -0500

    Add first Python solution for '169. Majority Element' problem

---
+++ easy/0169_majority_element/solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counter_map = {}
+
+        for num in nums:
+            counter_map[num] = counter_map.get(num, 0) + 1
+
+        return max(counter_map, key=counter_map.get)
