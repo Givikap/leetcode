a81bd2ccf6b5623b4fced311b506c24c4f4cccd5
Givikap <kaitosekiya@outlook.com>
2025-07-05 23:50:27 -0500

    Add second optimized solution for '202. Happy Number' problem

--- easy/0202_happy_number/solution.py
+++ easy/0202_happy_number/solution.py
@@ -2,15 +2,12 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         visited = set()
 
-        while True:
-            digit_square_sum = sum([int(digit) ** 2 for digit in str(n)])
+        while n != 1:
+            n = sum([int(digit) ** 2 for digit in str(n)])
 
-            if digit_square_sum == 1:
-                return True
-            elif digit_square_sum in visited:
+            if n in visited:
                 return False
 
-            n = digit_square_sum
-            visited.add(digit_square_sum)
-
-        return False
+            visited.add(n)
+        
+        return True
