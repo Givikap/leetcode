d9a6096dff2b51d689e35082830bd3ec07b008f1
Givikap <kaitosekiya@outlook.com>
2025-07-11 23:25:10 -0500

    Add first Python solution for '9. Palindrome Number' problem

---
+++ easy/0009_palindrome_number/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: 
+            return False
+        if x < 10:
+            return True
+
+        temp = x
+        reversed_x = 0
+
+        while temp:
+            reversed_x = reversed_x * 10 + temp % 10
+            temp //= 10
+
+        return x == reversed_x
