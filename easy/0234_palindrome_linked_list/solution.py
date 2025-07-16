c927605a2fb19464b46e74b716e5f8511e8e9a4e
Givikap <kaitosekiya@outlook.com>
2025-07-15 22:09:51 -0500

    Add first Python solution for '234. Palindrome Linked List' problem

---
+++ easy/0234_palindrome_linked_list/solution.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from utils.util import ListNode
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return True
+
+        slow = fast = head
+        prev = None
+
+        while fast and fast.next:
+            fast = fast.next.next
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+
+        if fast:
+            slow = slow.next
+
+        while slow:
+            if slow.val != prev.val:
+                return False
+
+            slow = slow.next
+            prev = prev.next
+
+        return True
