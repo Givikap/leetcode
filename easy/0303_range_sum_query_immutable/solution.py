a0af64be89d79648b746b12bbe79721ad11a11dc
Givikap <kaitosekiya@outlook.com>
2025-07-16 22:53:05 -0500

    Add second a bit faster but memory hungry Python solution for '303. Range Sum Query - Immutable' problem

--- easy/0303_range_sum_query_immutable/solution.py
+++ easy/0303_range_sum_query_immutable/solution.py
@@ -2,7 +2,13 @@ from typing import List
 
 class NumArray:
     def __init__(self, nums: List[int]):
-        self.range = nums
+        self.range = tuple(nums)
+        self.sumRanges = {}
 
     def sumRange(self, left: int, right: int) -> int:
-        return sum(self.range[left:right+1])
+        key = (left, right)
+
+        if key not in self.sumRanges:
+            self.sumRanges[key] = sum(self.range[left:right+1])
+
+        return self.sumRanges.get(key)
