3311e3668a74199cc5344ab7335018ba072e38cc
Givikap <kaitosekiya@outlook.com>
2025-07-29 22:43:35 -0500

    Add first solution for '145. Binary Tree Postorder Traversal' problem

---
+++ easy/0145_binary_tree_postorder_traversal/solution.py
@@ -0,0 +1,21 @@
+from typing import List, Optional
+from utils.util import TreeNode
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        stack = [root]
+        values = []
+        
+        while stack:
+            node = stack.pop()
+            values.append(node.val)
+
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        
+        return values[::-1]
