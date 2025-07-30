128f3b8d78d0317a3770bca15ff31cfef13a86b6
Givikap <kaitosekiya@outlook.com>
2025-07-29 22:23:08 -0500

    Add first solution for '94. Binary Tree Inorder Traversal' problem

---
+++ easy/0094_binary_tree_inorder_traversal/solution.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+from utils.util import TreeNode
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        values = []
+        
+        stack = []
+        current = root
+
+        while current or stack:
+            while current:
+                stack.append(current)
+                current = current.left
+            
+            current = stack.pop()
+            values.append(current.val)
+            current = current.right
+
+        return values
