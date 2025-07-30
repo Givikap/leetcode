bb09a03d20e2cd75db60d235017dc58a683f90e5
Givikap <kaitosekiya@outlook.com>
2025-07-29 22:28:55 -0500

    Add first solution for '144. Binary Tree Preorder Traversal' problem

---
+++ easy/0144_binary_tree_preorder_traversal/solution.py
@@ -0,0 +1,22 @@
+from collections import deque
+from typing import List, Optional
+from utils.util import TreeNode
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        values = []
+        if not root:
+            return values
+
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+            values.append(node.val)
+
+            if node.right:
+                queue.appendleft(node.right)
+            if node.left:
+                queue.appendleft(node.left)
+
+        return values
