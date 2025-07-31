26e3a4e2ab163a3ab5d3b49bd1ba9fbe8575f3a7
Givikap <kaitosekiya@outlook.com>
2025-07-30 22:16:20 -0500

    Add second faster solution for '1863. Sum of All Subset XOR Totals' problem

--- easy/1863_sum_of_all_subset_xor_totals/solution.py
+++ easy/1863_sum_of_all_subset_xor_totals/solution.py
@@ -1,18 +1,6 @@
-from itertools import combinations
 from functools import reduce
 from typing import List
 
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-
-        xorSum = sum(nums)
-
-        for l in range(2, len(nums) + 1):
-            xorSum += sum([
-                reduce(lambda x, y: x ^ y, subset)
-                for subset in combinations(nums, l)
-            ])
-
-        return xorSum     
+        return reduce(lambda x, y: x | y, nums) * (2 ** (len(nums) - 1))     
