9c09ec284dcf62943c1dc8dcbc631bbfdef6b74b
Givikap <kaitosekiya@outlook.com>
2025-08-04 02:12:00 -0500

    Add second faster Python solution for '69. Sqrt(x)' problem

---
+++ easy/0069_sqrt_x/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        left, right = 2, x // 2
+
+        while left <= right:
+            mid = (right + left) // 2
+            midSqrt = mid ** 2
+
+            if midSqrt == x:
+                return mid
+            elif midSqrt < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
