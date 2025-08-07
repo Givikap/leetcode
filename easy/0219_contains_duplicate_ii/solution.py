13fc0fa363e409896bfdb70a6b94a463ae42ecc8
Givikap <kaitosekiya@outlook.com>
2025-08-07 17:55:11 -0500

    Add second faster Python solution for '219. Contains Duplicate II' problem

--- easy/0219_contains_duplicate_ii/solution.py
+++ easy/0219_contains_duplicate_ii/solution.py
@@ -2,15 +2,22 @@ from typing import List
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        if k == 0:
+            return False
+
         visited = set()
 
-        for i, num in enumerate(nums):
-            if num in visited:
+        for i in range(min(k, len(nums))):
+            if nums[i] in visited:
                 return True
 
-            visited.add(num)
+            visited.add(nums[i])
+        
+        for i in range(k, len(nums)):
+            if nums[i] in visited:
+                return True
 
-            if len(visited) > k:
-                visited.remove(nums[i - k])
+            visited.add(nums[i])
+            visited.remove(nums[i - k])
         
         return False
