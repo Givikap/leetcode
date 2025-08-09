0485c335ff823f5ca57a1012aef3a12026581e1a
Givikap <kaitosekiya@outlook.com>
2025-08-08 22:14:12 -0500

    Add first Python solution for '232. Implement Queue Using Stacks' problem

---
+++ easy/0232_implement_queue_using_stacks/solution.py
@@ -0,0 +1,28 @@
+class MyQueue:
+    def __init__(self):
+        self.stack = []
+        self.queue = []
+        
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
+    def pop(self) -> int:
+        if self.queue:
+            return self.queue.pop()
+
+        while self.stack:
+            self.queue.append(self.stack.pop())
+
+        return self.queue.pop()
+        
+    def peek(self) -> int:
+        if self.queue:
+            return self.queue[-1]
+ 
+        while self.stack:
+            self.queue.append(self.stack.pop())
+
+        return self.queue[-1]
+        
+    def empty(self) -> bool:
+        return not self.stack and not self.queue
