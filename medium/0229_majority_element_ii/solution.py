64bdb0ed8e163d04ca04ff2fb26886413345e10f
Givikap <kaitosekiya@outlook.com>
2025-08-09 23:55:52 -0500

    Add second more space efficient Python solution for '229. Majority Element II' problem

--- medium/0229_majority_element_ii/solution.py
+++ medium/0229_majority_element_ii/solution.py
@@ -3,5 +3,29 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        target = len(nums) / 3
-        return [num for num, count in Counter(nums).items() if count > target]
+        m1, m2 = None, None
+        c1, c2 = 0, 0
+
+        for num in nums:
+            if num == m1:
+                c1 += 1
+            elif num == m2:
+                c2 += 1
+            elif c1 == 0:
+                m1 = num
+                c1 = 1
+            elif c2 == 0:
+                m2 = num
+                c2 = 1
+            else: 
+                c1 -= 1
+                c2 -= 1
+
+        majorities = []
+
+        if nums.count(m1) > len(nums) // 3:
+            majorities.append(m1)
+        if m2 is not None and nums.count(m2) > len(nums) // 3:
+            majorities.append(m2)
+
+        return majorities
