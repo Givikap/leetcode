a38baff86f6983fb8b74d5b4305f5d4d44b77830
Givikap <kaitosekiya@outlook.com>
2025-08-11 22:57:00 -0500

    Add second faster Python solution for '3110. Score of a string' problem

--- easy/3110_score_of_a_string/solution.py
+++ easy/3110_score_of_a_string/solution.py
@@ -1,3 +1,4 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        return sum([abs(ord(s[i]) - ord(s[i+1])) for i in range(len(s) - 1)])
+        sValues = [ord(c) for c in s]
+        return sum([abs(sValues[i] - sValues[i+1]) for i in range(len(s) - 1)])
