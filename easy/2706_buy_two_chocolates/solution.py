b25551d0f7419596d8b56ad148f16693789a97fa
Givikap <kaitosekiya@outlook.com>
2025-08-12 23:05:28 -0500

    Add second optimized Python solution for '2706. Buy Two Chocolates' problem

--- easy/2706_buy_two_chocolates/solution.py
+++ easy/2706_buy_two_chocolates/solution.py
@@ -1,10 +1,9 @@
+import heapq
 from typing import List
 
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        twoCheapestChocoPrice = sum(sorted(prices)[:2])
-        
-        if twoCheapestChocoPrice <= money:
-            return money - twoCheapestChocoPrice
-        else:
-            return money
+        heapq.heapify(prices)
+
+        minTwoChocos = heapq.heappop(prices) + heapq.heappop(prices)
+        return money - minTwoChocos if minTwoChocos <= money else money
