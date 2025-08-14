cebdf3972308bff6dcab597e5d1c9261f6e52747
Givikap <kaitosekiya@outlook.com>
2025-08-14 15:24:58 -0500

    Add second Python solution for '2331. Evaluate Boolean Binary Tree' problem

--- easy/2331_evaluate_boolean_binary_tree/solution.py
+++ easy/2331_evaluate_boolean_binary_tree/solution.py
@@ -5,7 +5,8 @@ class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
         if root.val <= 1:
             return bool(root.val)
-        elif root.val == 2:
+        
+        if root.val == 2:
             return self.evaluateTree(root.left) or self.evaluateTree(root.right)
         else:
             return self.evaluateTree(root.left) and self.evaluateTree(root.right)
