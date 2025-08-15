16eb791d7494b64a865e2b1179ef0ec6fe870eaa
Givikap <kaitosekiya@outlook.com>
2025-08-15 00:15:40 -0500

    Add first solution for '203. Remove Linked List Elements' problem

---
+++ easy/0203_remove_linked_list_elements/solution.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from utils.util import ListNode
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        curr = head
+
+        while curr:
+            if curr.next and curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+
+        if head and head.val == val:
+            head = head.next
+
+        return head
