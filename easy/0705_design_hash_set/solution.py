bbb63a02f5202cff1312cb78ac12734e3dbdd12a
Givikap <kaitosekiya@outlook.com>
2025-08-15 23:22:17 -0500

    Add first solution for '705. Design HashSet' problem

---
+++ easy/0705_design_hash_set/solution.py
@@ -0,0 +1,12 @@
+class MyHashSet:
+    def __init__(self):
+        self.set = [False] * (10 ** 6 + 1)
+
+    def add(self, key: int) -> None:
+        self.set[key] = True
+
+    def remove(self, key: int) -> None:
+        self.set[key] = False
+
+    def contains(self, key: int) -> bool:
+        return self.set[key]
