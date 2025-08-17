c13499c8221021887cb8d3db1d8aa1bab48a77e7
Givikap <kaitosekiya@outlook.com>
2025-08-17 00:14:15 -0500

    Add second optimized solution for '304. Range Sum Query 2D - Immutable' problem

--- medium/0304_range_sum_query_2d_immutable/solution.py
+++ medium/0304_range_sum_query_2d_immutable/solution.py
@@ -2,16 +2,21 @@ from typing import List
 
 class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
-        self.prefixSumsMatrix = []
+        self.prefixSumsMatrix = [[0] * (len(matrix[0]) + 1)]
 
         for row in matrix:
             self.prefixSumsMatrix.append([0])
 
-            for num in row:
-                self.prefixSumsMatrix[-1].append(self.prefixSumsMatrix[-1][-1] + num)
+            for i, num in enumerate(row):
+                self.prefixSumsMatrix[-1].append(self.prefixSumsMatrix[-1][i] + num)
+
+            for i in range(1, len(row) + 1):
+                self.prefixSumsMatrix[-1][i] += self.prefixSumsMatrix[-2][i]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return sum([
-            self.prefixSumsMatrix[row][col2 + 1] - self.prefixSumsMatrix[row][col1] 
-            for row in range(row1, row2 + 1)
-        ])
+        return (
+            self.prefixSumsMatrix[row2+1][col2+1] -
+            self.prefixSumsMatrix[row2+1][col1] -
+            self.prefixSumsMatrix[row1][col2+1] +
+            self.prefixSumsMatrix[row1][col1]
+        )
