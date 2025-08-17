73c7d56243bcf4fb1e8d90f7c5598bfc06354d6f
Givikap <kaitosekiya@outlook.com>
2025-08-17 13:03:41 -0500

    Add first solution for '179. Largest Number' problem

---
+++ medium/0179_largest_number/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        result = "".join(
+            sorted(
+                map(str, nums), 
+                key=lambda num: num * 10,
+                reverse=True
+            )
+        )
+
+        return "0" if result[0] == "0" else result
