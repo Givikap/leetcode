d8445078c26755cccb125ea7ddc975884d754e9a
Givikap <kaitosekiya@outlook.com>
2025-08-19 00:25:05 -0500

    Add first Python solution for '2558. Take Gifts from the Richest Pile' problem

---
+++ easy/2558_take_gifts_from_the_richest_pile/solution.py
@@ -0,0 +1,13 @@
+import heapq
+import math
+from typing import List
+
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        gifts = [-gift for gift in gifts]
+        heapq.heapify(gifts)
+
+        for _ in range(k):
+            heapq.heappush(gifts, -(int(math.sqrt(-heapq.heappop(gifts)))))
+
+        return -sum(gifts)
