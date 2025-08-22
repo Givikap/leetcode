3b4e017b537c51766397094cd3058ef4aa4e0650
Givikap <kaitosekiya@outlook.com>
2025-08-22 01:11:15 -0500

    Add second non-recursive Python solution for '108. Convert Sorted Array to Binary Search Tree' problem

--- easy/0108_convert_sorted_array_to_binary_search_tree/solution.py
+++ easy/0108_convert_sorted_array_to_binary_search_tree/solution.py
@@ -6,10 +6,20 @@ class Solution:
         mid = len(nums) // 2
 
         root = TreeNode(val=nums[mid])
+        stack = [(root, nums[:mid], nums[mid+1:])]
 
-        if mid > 0:
-            root.left = self.sortedArrayToBST(nums[:mid])
-        if mid + 1 < len(nums):
-            root.right = self.sortedArrayToBST(nums[mid+1:])
+        while stack:
+            node, left, right = stack.pop()
+
+            if left:
+                mid = len(left) // 2
+
+                node.left = TreeNode(val=left[mid])
+                stack.append((node.left, left[:mid], left[mid+1:]))
+            if right:
+                mid = len(right) // 2
+
+                node.right = TreeNode(val=right[mid])
+                stack.append((node.right, right[:mid], right[mid+1:]))
 
         return root
