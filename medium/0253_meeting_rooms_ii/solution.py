dfaa69eb5e299ac370cabf14e11390a676c2b85c
Givikap <kaitosekiya@outlook.com>
2025-08-23 23:53:26 -0500

    Add first Python solution for '253. Meeting Rooms II' problem

---
+++ medium/0253_meeting_rooms_ii/solution.py
@@ -0,0 +1,21 @@
+import heapq
+from typing import List
+from utils.interval import Interval
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda interval: interval.start)
+
+        heap = []
+        max_rooms = 0
+
+        for interval in intervals:
+            if heap and interval.start >= heap[0]:
+                heapq.heappop(heap)
+                
+            heapq.heappush(heap, interval.end)
+
+            if len(heap) > max_rooms:
+                max_rooms = len(heap)
+
+        return max_rooms
