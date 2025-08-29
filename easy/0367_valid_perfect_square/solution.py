2b48760b30bdad3ee591cb82b187fd6ce7ebcf02
Givikap <kaitosekiya@outlook.com>
2025-08-29 01:01:44 -0500

    Add first Python solution for '367. Valid Perfect Square' problem

---
+++ easy/0367_valid_perfect_square/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num == 1:
+            return True
+        
+        left, right = 0, num
+
+        while left <= right:
+            mid = (left + right) // 2
+            mid_squared = mid ** 2
+
+            if mid_squared == num:
+                return True
+            elif mid_squared < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
+    
