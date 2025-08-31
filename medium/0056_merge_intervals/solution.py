28ae98129e4b4118ed6b6e6ebf454a3839bd55d5
Givikap <kaitosekiya@outlook.com>
2025-08-31 14:11:47 -0500

    Add second faster Python solution for '56. Merge Intervals' problem

--- medium/0056_merge_intervals/solution.py
+++ medium/0056_merge_intervals/solution.py
@@ -6,17 +6,15 @@ class Solution:
         heapq.heapify(intervals)
         merged_intervals = []
 
-        while len(intervals) > 1:
-            interval1 = heapq.heappop(intervals)
-            interval2 = intervals[0]
+        curr = heapq.heappop(intervals)
+        while intervals:
+            next = heapq.heappop(intervals)
 
-            if interval1[1] >= interval2[0]:
-                heapq.heappop(intervals)
-                heapq.heappush(intervals, [interval1[0], max(interval1[1], interval2[1])])
+            if curr[1] >= next[0]:
+                curr[1] = max(curr[1], next[1])
             else:
-                merged_intervals.append(interval1)
+                merged_intervals.append(curr)
+                curr = next
 
-        if intervals:
-            merged_intervals.append(intervals[0])
-
+        merged_intervals.append(curr)
         return merged_intervals
