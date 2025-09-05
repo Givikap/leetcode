5697eb2b392a0264bba4fa8d72ed7a5a1ce77416
Givikap <kaitosekiya@outlook.com>
2025-09-05 00:59:17 -0500

    Add first Python solution for '530. Minimum Absolute Difference in BST' problem

---
+++ easy/0530_minimum_absolute_difference_in_bst/solution.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from utils.nodes import TreeNode
+
+
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        stack = []
+        values = []
+
+        curr = root
+
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+
+            curr = stack.pop()
+            values.append(curr.val)
+            curr = curr.right
+
+        return min([abs(values[i] - values[i + 1]) for i in range(len(values) - 1)])
