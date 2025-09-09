74f317dfe54b5c8894f84cbc1450dcee1a148c02
Givikap <kaitosekiya@outlook.com>
2025-09-09 11:57:15 -0500

    Add first Python solution for '1137. N-th Tribonacci Number' problem

---
+++ easy/1137_nth_tribonacci_number/solution.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+
+
+class Solution:
+    @lru_cache(maxsize=64)
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n < 3:
+            return 1
+
+        return (
+            self.tribonacci(n - 1)
+            + self.tribonacci(n - 2)
+            + self.tribonacci(n - 3)
+        )
