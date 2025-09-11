9eb7f9ad4aed2a3d7c477d59082769aac944dc02
Givikap <kaitosekiya@outlook.com>
2025-09-11 00:26:28 -0500

    Add second actual Python solution for '709. To Lower Case' problem

--- easy/0709_to_lower_case/solution.py
+++ easy/0709_to_lower_case/solution.py
@@ -1,3 +1,5 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        return "".join(
+            [chr(ord(c) + 32) if 64 < ord(c) < 91 else c for c in s]
+        )
