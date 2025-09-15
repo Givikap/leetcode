e7780c1102e45c56eefb9d8d55bfd427c2f87001
Givikap <kaitosekiya@outlook.com>
2025-09-15 01:22:05 -0500

    Add second optimized Python solution for '1365. How Many Numbers Are Smaller Than the Current Number' problem

--- easy/1365_how_many_numbers_are_smaller_than_the_current_number/solution.py
+++ easy/1365_how_many_numbers_are_smaller_than_the_current_number/solution.py
@@ -3,12 +3,9 @@ from typing import List
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        ranked_nums = [[num, i] for i, num in enumerate(sorted(nums))]
+        num_to_rank = {}
 
-        for i in range(len(ranked_nums) - 1):
-            if ranked_nums[i][0] == ranked_nums[i + 1][0]:
-                ranked_nums[i + 1][1] = ranked_nums[i][1]
+        for i, num in enumerate(sorted(nums)):
+            num_to_rank.setdefault(num, i)
 
-        num_to_smaller_count = dict(ranked_nums)
-
-        return [num_to_smaller_count[num] for num in nums]
+        return [num_to_rank[num] for num in nums]
