0afe35478225af80bef138b0c8dfd1e4b89e2877
Givikap <kaitosekiya@outlook.com>
2025-09-16 16:21:13 -0500

    Add second optimized solution for '2839. Check if Strings Can be Made Equal With Operations I' problem

--- easy/2839_check_if_strings_can_be_made_equal_with_operations_i/solution.py
+++ easy/2839_check_if_strings_can_be_made_equal_with_operations_i/solution.py
@@ -1,8 +1,6 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
         return (
-            s1[0] in (s2[0], s2[2])
-            and s1[1] in (s2[1], s2[3])
-            and s1[2] in (s2[0], s2[2])
-            and s1[3] in (s2[1], s2[3])
+            set(s1[::2]).issubset({s2[0], s2[2]}) 
+            and set(s1[1::2]).issubset({s2[1], s2[3]})
         )
