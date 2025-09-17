877257e6e8febbc9eb78731b8b58824c541fdbd3
Givikap <kaitosekiya@outlook.com>
2025-09-17 00:35:29 -0500

    Add second optimized Python solution for '2529. Maximum Count of Positive and Negative Number' problem

--- easy/2529_maximum_count_of_positive_integer_and_negative_integer/solution.py
+++ easy/2529_maximum_count_of_positive_integer_and_negative_integer/solution.py
@@ -6,10 +6,16 @@ class Solution:
         positive_count = 0
         negative_count = 0
 
-        for num in nums:
-            if num > 0:
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] > 0:
                 positive_count += 1
-            elif num < 0:
+            else:
+                break
+
+        for i in range(len(nums)):
+            if nums[i] < 0:
                 negative_count += 1
+            else:
+                break
 
         return max(positive_count, negative_count)
