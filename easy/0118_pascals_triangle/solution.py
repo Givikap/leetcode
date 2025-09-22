d7f8257f1d6a0580def888142065cf626f712b26
Givikap <kaitosekiya@outlook.com>
2025-09-22 17:24:30 -0500

    Add second nicer Python solution for '118. Pascal's Triangle' problem

--- easy/0118_pascals_triangle/solution.py
+++ easy/0118_pascals_triangle/solution.py
@@ -3,18 +3,14 @@ from typing import List
 
 class Solution:
     def generate(self, num_rows: int) -> List[List[int]]:
-        rows = [[1]]
+        rows = []
 
-        if num_rows > 1:
-            rows.append([1, 1])
+        for r in range(num_rows):
+            row = [1] * (r + 1)
 
-        for _ in range(num_rows - 2):
-            row = [1]
+            for i in range(1, r):
+                row[i] = rows[-1][i - 1] + rows[-1][i]
 
-            for i in range(len(rows[-1]) - 1):
-                row.append(rows[-1][i] + rows[-1][i + 1])
-
-            row.append(1)
             rows.append(row)
 
         return rows
