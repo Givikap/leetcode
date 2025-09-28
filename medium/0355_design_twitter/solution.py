3661af6337db52bba64f9f14ccee49aba4c6eedd
Givikap <kaitosekiya@outlook.com>
2025-09-28 01:39:37 -0500

    Add third more optimized Python solution for '355. Design Twitter' problem

--- medium/0355_design_twitter/solution.py
+++ medium/0355_design_twitter/solution.py
@@ -1,63 +1,56 @@
 import heapq
+from collections import defaultdict
 from typing import List
 
 
 class Twitter:
     def __init__(self):
-        self.tweets: dict[int, list[tuple[int, int]]] = {}
-        self.follows: dict[int, set] = {}
+        self.tweets = defaultdict(list)
+        self.follows = defaultdict(set)
         self.time = 0
 
     def postTweet(self, user_id: int, tweet_id: int) -> None:
-        if user_id not in self.tweets:
-            self.tweets[user_id] = []
-
-        self.tweets[user_id].append((self.time, tweet_id))
+        self.tweets[user_id].append((-self.time, tweet_id))
         self.time += 1
 
     def getNewsFeed(self, user_id: int) -> List[int]:
-        feeds = []
+        heap = []
 
-        if user_id in self.tweets:
-            feeds.append((user_id, -1))
-
-        if user_id in self.follows:
-            feeds.extend(
-                [
-                    (followee_id, -1)
-                    for followee_id in self.follows[user_id]
-                    if followee_id in self.tweets
-                ]
+        if self.tweets[user_id]:
+            heap.append(
+                (
+                    *self.tweets[user_id][-1],
+                    user_id,
+                    len(self.tweets[user_id]) - 1,
+                )
             )
 
-        tweets = []
-
-        heap = [
-            (*self.tweets[user_id][ri], user_id, ri) for user_id, ri in feeds
-        ]
-        heapq.heapify(heap)
-
-        for _ in range(10):
-            if not heap:
-                break
-
-            _, tweet_id, user_id, ri = heapq.heappop(heap)
-
-            tweets.append(tweet_id)
-
-            if len(self.tweets[user_id]) > -ri:
-                heapq.heappush(
-                    heap, (*self.tweets[user_id][ri - 1], user_id, ri - 1)
+        for followee in self.follows[user_id]:
+            if self.tweets[followee]:
+                heap.append(
+                    (
+                        *self.tweets[followee][-1],
+                        followee,
+                        len(self.tweets[followee]) - 1,
+                    )
                 )
 
-        return tweets
+        heapq.heapify(heap)
+        news_feed = []
+
+        while heap and len(news_feed) < 10:
+            _, tweet_id, user_id, i = heapq.heappop(heap)
+            news_feed.append(tweet_id)
+
+            if i > 0:
+                heapq.heappush(
+                    heap, (*self.tweets[user_id][i - 1], user_id, i - 1)
+                )
+
+        return news_feed
 
     def follow(self, follower_id: int, followee_id: int) -> None:
-        if follower_id not in self.follows:
-            self.follows[follower_id] = set()
-
         self.follows[follower_id].add(followee_id)
 
     def unfollow(self, follower_id: int, followee_id: int) -> None:
-        if follower_id in self.follows:
-            self.follows[follower_id].remove(followee_id)
+        self.follows[follower_id].discard(followee_id)
