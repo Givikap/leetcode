7ddd802933bb298fe5024e0a1c4252c305ef2271
Givikap <kaitosekiya@outlook.com>
2025-09-30 14:33:03 -0500

    Add second faster solution for '3042. Count Prefix and Suffix Pairs I' problem

--- easy/3042_count_prefix_and_suffix_pairs_i/solution.py
+++ easy/3042_count_prefix_and_suffix_pairs_i/solution.py
@@ -2,14 +2,11 @@ from typing import List
 
 
 class Solution:
-    def isPrefixAndSuffix(self, str1: str, str2: str) -> bool:
-        return str2.startswith(str1) and str2.endswith(str1)
-
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
         counter = 0
 
         for i in range(len(words) - 1):
             for j in range(i + 1, len(words)):
-                counter += self.isPrefixAndSuffix(words[i], words[j])
+                counter += words[j].startswith(words[i]) and words[j].endswith(words[i])
 
         return counter
