2f9619dddd5652cb0b084bf6ec171a4f7f1d3588
Givikap <kaitosekiya@outlook.com>
2025-10-04 14:25:11 -0500

    Add second faster Python solution for '973. K Closest Points to Origin' problem

--- medium/0973_k_closest_points_to_origin/solution.py
+++ medium/0973_k_closest_points_to_origin/solution.py
@@ -4,15 +4,15 @@ from typing import List
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        heap = [
-            ((point[0] ** 2 + point[1] ** 2) ** 0.5, i)
-            for i, point in enumerate(points)
-        ]
-        heapq.heapify(heap)
-
+        heap = []
         closest_points = []
 
-        for _ in range(k):
+        for i, point in enumerate(points):
+            heap.append(((point[0] ** 2 + point[1] ** 2) ** 0.5, i))
+
+        heapq.heapify(heap)
+
+        for i in range(k):
             closest_points.append(points[heapq.heappop(heap)[1]])
 
         return closest_points
