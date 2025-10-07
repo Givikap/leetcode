06905ce35f13c348285f518fdc132d611a647c15
Givikap <kaitosekiya@outlook.com>
2025-10-06 23:48:06 -0500

    Add first solution for '101. Symmetric Tree' problem

---
+++ easy/0101_symmetric_tree/solution.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from utils.nodes import TreeNode
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        stack = [(root.left, root.right)]
+
+        while stack:
+            left, right = stack.pop()
+
+            if not left and not right:
+                continue
+
+            if not left or not right or left.val != right.val:
+                return False
+
+            stack.append((left.left, right.right))
+            stack.append((left.right, right.left))
+
+        return True
