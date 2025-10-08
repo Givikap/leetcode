ec04ebdad88ce0a1c35940b8dd3e9acff3d6a850
Givikap <kaitosekiya@outlook.com>
2025-10-07 20:44:46 -0500

    Add first solution for '374. Guess Number Higher or Lower' problem

---
+++ easy/0374_guess_number_higher_or_lower/solution.py
@@ -0,0 +1,21 @@
+def guess(num: int) -> int:
+    pass
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left = 1
+        right = n
+
+        while left < right:
+            mid = (left + right) // 2
+            res = guess(mid)
+
+            if res == 0:
+                return mid
+            elif res == -1:
+                right = mid
+            else:
+                left = mid + 1
+
+        return right
