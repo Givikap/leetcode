6d29482e95107432f722c5fc408d460e3ebfe7d0
Givikap <kaitosekiya@outlook.com>
2025-10-11 23:08:37 -0500

    Add first Python solution for '48. Rotate Image' problem

---
+++ medium/0048_rotate_image/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        for row in range(len(matrix) - 1):
+            for col in range(row + 1, len(matrix)):
+                matrix[row][col], matrix[col][row] = (
+                    matrix[col][row],
+                    matrix[row][col],
+                )
+
+        for row in range(len(matrix)):
+            for col in range(len(matrix) // 2):
+                matrix[row][col], matrix[row][-col - 1] = (
+                    matrix[row][-col - 1],
+                    matrix[row][col],
+                )
