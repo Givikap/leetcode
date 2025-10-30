73965a7d68361ced9f5125e39ad47dee86d5ef4b
Givikap <kaitosekiya@outlook.com>
2025-10-30 14:15:31 -0500

    Add first solution for '404. Sum of Left Leaves' problem

---
+++ easy/0404_sum_of_left_leaves/solution.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from utils.nodes import TreeNode
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        stack = [(root, False)]
+        left_leaves_sum = 0
+
+        while stack:
+            node, is_left = stack.pop()
+
+            if not node.left and not node.right and is_left:
+                left_leaves_sum += node.val
+
+            if node.left:
+                stack.append((node.left, True))
+            if node.right:
+                stack.append((node.right, False))
+
+        return left_leaves_sum
