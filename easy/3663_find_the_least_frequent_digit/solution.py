624c9cae326e4cd81a7f48eedd042b5eaca06a81
Givikap <kaitosekiya@outlook.com>
2025-11-15 00:58:36 -0600

    Add second faster Python solution for '3663. Find the Least Frequent Digit' problem

--- easy/3663_find_the_least_frequent_digit/solution.py
+++ easy/3663_find_the_least_frequent_digit/solution.py
@@ -1,6 +1,12 @@
-from collections import Counter
+from collections import defaultdict
 
 
 class Solution:
     def getLeastFrequentDigit(self, n: int) -> int:
-        return int(min(Counter(str(n)).items(), key=lambda t: (t[1], t[0]))[0])
+        digits_counter = defaultdict(int)
+
+        while n:
+            digits_counter[n % 10] += 1
+            n //= 10
+
+        return min(digits_counter.items(), key=lambda t: (t[1], t[0]))[0]
