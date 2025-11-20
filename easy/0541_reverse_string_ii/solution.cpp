1cb64cb86be21a423a49e2ddb777e40445fab6be
Givikap <kaitosekiya@outlook.com>
2025-11-20 11:15:34 -0600

    Add second faster C++ solution for '541. Reverse String II' problem

--- easy/0541_reverse_string_ii/solution.cpp
+++ easy/0541_reverse_string_ii/solution.cpp
@@ -1,34 +1,14 @@
 using namespace std;
 
+#include <algorithm>
 #include <string>
 
 class Solution {
 public:
   string reverseStr(string s, int k) {
-    string reversedS;
+    for (int i = 0; i < s.size(); i += 2 * k)
+      reverse(begin(s) + i, begin(s) + min(i + k, (int)s.size()));
 
-    int start = 0;
-    int end = min(k - 1, (int)s.size() - 1);
-
-    int i;
-
-    while (true) {
-      for (i = end; i > start - 1; i--)
-        reversedS.push_back(s[i]);
-
-      start = end + 1;
-      end = min(end + k + 1, (int)s.size());
-
-      if (start >= s.size())
-        break;
-
-      for (i = start; i < end; i++)
-        reversedS.push_back(s[i]);
-
-      start = end;
-      end = min(start + k - 1, (int)s.size() - 1);
-    }
-
-    return reversedS;
+    return s;
   }
 };
