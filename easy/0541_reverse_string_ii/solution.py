8a3e8e55a32889bc1b511be17a70b17ceaf7df3f
Givikap <kaitosekiya@outlook.com>
2025-11-20 11:19:43 -0600

    Add third nicer Python solution for '541. Reverse String II' problem

--- easy/0541_reverse_string_ii/solution.py
+++ easy/0541_reverse_string_ii/solution.py
@@ -1,24 +1,8 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        reversed_s = []
+        s = list(s)
 
-        start = 0
-        end = min(k - 1, len(s) - 1)
+        for i in range(0, len(s), 2 * k):
+            s[i : i + k] = reversed(s[i : i + k : -1])
 
-        while True:
-            for i in range(end, start - 1, -1):
-                reversed_s += s[i]
-
-            start = end + 1
-            end = min(end + k + 1, len(s))
-
-            if start >= len(s):
-                break
-
-            for i in range(start, end):
-                reversed_s.append(s[i])
-
-            start = end
-            end = min(start + k - 1, len(s) - 1)
-
-        return "".join(reversed_s)
+        return "".join(s)
