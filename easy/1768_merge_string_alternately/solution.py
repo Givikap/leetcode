dc8a202247e04457335f158bce92224ed09d50dc
Givikap <kaitosekiya@outlook.com>
2025-11-23 02:00:39 -0600

    Add second Python solution for '1768. Merge Strings Alternately' problem

--- easy/1768_merge_string_alternately/solution.py
+++ easy/1768_merge_string_alternately/solution.py
@@ -2,8 +2,10 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         new_word = []
 
-        for i in range(min(len(word1), len(word2))):
-            new_word.extend((word1[i], word2[i]))
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                new_word.append(word1[i])
+            if i < len(word2):
+                new_word.append(word2[i])
 
-        longer_word = word1 if len(word1) > len(word2) else word2
-        return "".join(new_word) + longer_word[min(len(word1), len(word2)) :]
+        return "".join(new_word)
