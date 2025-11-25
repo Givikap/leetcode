cdeb7f6d870788499c045ee26b4cabd6c5256058
Givikap <kaitosekiya@outlook.com>
2025-11-24 20:12:50 -0600

    Add first C++ solution for '1426. Counting Elements' problem

---
+++ easy/1426_counting_elements/solution.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+public:
+  int countElements(vector<int> &arr) {
+    unordered_set<int> numsSet(arr.begin(), arr.end());
+    int elementsCount = 0;
+
+    for (int &num : arr) {
+      if (numsSet.find(num + 1) != numsSet.end())
+        elementsCount++;
+    }
+
+    return elementsCount;
+  }
+};
