4626b7c41e17c846bb1f73cc2d094ea9172f664a
Givikap <kaitosekiya@outlook.com>
2025-11-29 15:58:17 -0600

    Add third faster Python solution for '1018. Binary Prefix Divisible By 5' problem

--- easy/1018_binary_prefix_divisible_by_5/solution.py
+++ easy/1018_binary_prefix_divisible_by_5/solution.py
@@ -3,11 +3,10 @@ from typing import List
 
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
-        bin_num = 0
-        answer = []
+        val = 0
 
         for i in range(len(nums)):
-            bin_num = (bin_num << 1) | nums[i]
-            answer.append(bin_num % 5 == 0)
+            val = (val * 2 + nums[i]) % 5
+            nums[i] = val == 0
 
-        return answer
+        return nums
