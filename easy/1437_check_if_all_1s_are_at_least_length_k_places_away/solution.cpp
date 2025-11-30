b9782f98e2c6051c3120885b60e4ce02f003208b
Givikap <kaitosekiya@outlook.com>
2025-11-30 01:35:14 -0600

    Add first C++ solution for '1437. Check If All 1's Are at Least Length K Places Away' problem

---
+++ easy/1437_check_if_all_1s_are_at_least_length_k_places_away/solution.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+  bool kLengthApart(vector<int> &nums, int k) {
+    int zerosCount = 1000001;
+
+    for (int &num : nums) {
+      if (num == 1) {
+        if (zerosCount < k)
+          return false;
+        zerosCount = 0;
+      } else {
+        zerosCount++;
+      }
+    }
+
+    return true;
+  }
+};
