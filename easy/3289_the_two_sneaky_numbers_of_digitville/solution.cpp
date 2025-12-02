33fa6409b9431d8963a6ed9085d430de0a8cf349
Givikap <kaitosekiya@outlook.com>
2025-12-02 01:23:10 -0600

    Add second nicer C++ solution for '3289. The Two Sneaky Numbers of Digitville' problem

--- easy/3289_the_two_sneaky_numbers_of_digitville/solution.cpp
+++ easy/3289_the_two_sneaky_numbers_of_digitville/solution.cpp
@@ -1,22 +1,22 @@
 using namespace std;
 
-#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 class Solution {
 public:
   vector<int> getSneakyNumbers(vector<int> &nums) {
-    unordered_map<int, int> numsCounter;
+    unordered_set<int> numsSet;
     vector<int> sneakyNums;
 
     for (int &num : nums) {
-      numsCounter[num] += 1;
-
-      if (numsCounter[num] == 2) {
+      if (numsSet.find(num) != numsSet.end()) {
         sneakyNums.push_back(num);
 
         if (sneakyNums.size() == 2)
-          return sneakyNums;
+          break;
+      } else {
+        numsSet.insert(num);
       }
     }
 
