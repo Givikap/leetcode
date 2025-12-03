885a69485a92c0935b449eea511eb08866445e2e
Givikap <kaitosekiya@outlook.com>
2025-12-03 00:47:36 -0600

    Add second faster Python solution for '1716. Calculate Money in Leetcode Bank' problem

--- easy/1716_calculate_money_in_leetcode_bank/solution.py
+++ easy/1716_calculate_money_in_leetcode_bank/solution.py
@@ -1,15 +1,7 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        money_sum = 0
-
-        for week in range(n // 7 + 1):
-            days_in_week = min(n, 7)
-
-            for money in range(1, days_in_week + 1):
-                money_sum += money
-
-            money_sum += days_in_week * week
-
-            n -= min(n, 7)
-
-        return money_sum
+        return sum(
+            7 * week + 28 for week in range(n // 7)
+        ) + sum(
+            day + n // 7 for day in range(1, n % 7 + 1)
+        )
