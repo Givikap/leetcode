f849385832024f9d10167b973c6a3d75a589b979
Givikap <kaitosekiya@outlook.com>
2025-12-06 02:17:12 -0600

    Add third nicer Python solution for '3005. Count Elements With Maximum Frequency' problem

--- easy/3005_count_elements_with_maximum_frequency/solution.py
+++ easy/3005_count_elements_with_maximum_frequency/solution.py
@@ -5,16 +5,17 @@ from typing import List
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         nums_counter = defaultdict(int)
+
         max_frequency = 0
+        max_frequency_count = 0
 
         for num in nums:
             nums_counter[num] += 1
 
             if nums_counter[num] > max_frequency:
                 max_frequency = nums_counter[num]
+                max_frequency_count = 1
+            elif nums_counter[num] == max_frequency:
+                max_frequency_count += 1
 
-        return sum(
-            frequency
-            for frequency in nums_counter.values()
-            if frequency == max_frequency
-        )
+        return max_frequency * max_frequency_count
