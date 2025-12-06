58045934068c41be95ccd30fb01b529402d1754f
Givikap <kaitosekiya@outlook.com>
2025-12-05 23:57:06 -0600

    Add first Python solution for '1518. Water Bottles' problem

---
+++ easy/1518_water_bottles/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numWaterBottles(self, num_bottles: int, num_exchanges: int) -> int:
+        total_num_bottles = num_empty_bottles = 0
+
+        while num_bottles:
+            total_num_bottles += num_bottles
+            num_empty_bottles += num_bottles
+
+            tmp = num_empty_bottles // num_exchanges
+            num_bottles = tmp
+            num_empty_bottles -= tmp * num_exchanges
+
+        return total_num_bottles
