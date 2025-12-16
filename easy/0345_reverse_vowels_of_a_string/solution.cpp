ee2985f548715e10807cefca3e1955478eb6c34e
Givikap <kaitosekiya@outlook.com>
2025-12-16 01:55:25 -0600

    Add first C++ solution for '345. Reverse Vowels of a String' problem

---
+++ easy/0345_reverse_vowels_of_a_string/solution.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+
+#include <string>
+
+class Solution {
+public:
+  bool isVowel(char c) {
+    c = tolower(c);
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+  }
+
+  string reverseVowels(string s) {
+    int left = 0;
+    int right = s.size() - 1;
+
+    while (left < right) {
+      if (!isVowel(s[left]))
+        ++left;
+      else if (!isVowel(s[right]))
+        --right;
+      else
+        swap(s[left++], s[right--]);
+    }
+
+    return s;
+  }
+};
