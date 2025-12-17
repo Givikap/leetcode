c2a0749dd0613ba9f335f6fda443a49c69eca754
Givikap <kaitosekiya@outlook.com>
2025-12-16 21:23:42 -0600

    Add third faster Python solution for '389. Find the Difference' problem

--- easy/0389_find_the_difference/solution.py
+++ easy/0389_find_the_difference/solution.py
@@ -1,11 +1,8 @@
-from collections import Counter
-
-
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s_counter = Counter(s)
-        t_counter = Counter(t)
+        diff = 0
 
-        for c in t_counter:
-            if c not in s_counter or s_counter[c] != t_counter[c]:
-                return c
+        for c in s + t:
+            diff ^= ord(c)
+
+        return chr(diff)
