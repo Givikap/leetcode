e4d5f21f122a0a7dd39b346b036d9ec403e5224f
Givikap <kaitosekiya@outlook.com>
2025-12-16 22:14:22 -0600

    Add second faster Python solution for '2147. Number of Ways to Divide a Long Corridor' problem

--- hard/2147_number_of_ways_to_divide_a_long_corridor/solution.py
+++ hard/2147_number_of_ways_to_divide_a_long_corridor/solution.py
@@ -1,28 +1,26 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        sections = []
-
         section_start = -1
-        i = 0
-
-        while i < len(corridor):
-            if corridor[i] == "S":
-                if section_start == -1:
-                    section_start = i
-                else:
-                    sections.append((section_start, i))
-                    section_start = -1
-
-            i += 1
-
-        if section_start != -1:
-            return 0
-        elif len(sections) < 2:
-            return len(sections)
+        prev_section_end = -1
 
         number_of_ways = 1
 
-        for i in range(len(sections) - 1):
-            number_of_ways *= sections[i + 1][0] - sections[i][1]
+        for i, item in enumerate(corridor):
+            if item != "S":
+                continue
+
+            if section_start == -1:
+                section_start = i
+
+                if prev_section_end != -1:
+                    number_of_ways *= section_start - prev_section_end
+            else:
+                prev_section_end = i
+                section_start = -1
+
+        if section_start != -1:
+            return 0
+        elif prev_section_end == -1:
+            return 0
 
         return number_of_ways % (10**9 + 7)
