3ef0d141ccfa9e2eea1f209affba6c159b7770ac
Givikap <kaitosekiya@outlook.com>
2025-12-19 03:12:55 -0600

    Add second faster C++ solution for '4. Median of Two Sorted Arrays' problem

--- hard/0004_median_of_two_sorted_arrays/solution.cpp
+++ hard/0004_median_of_two_sorted_arrays/solution.cpp
@@ -5,33 +5,23 @@ using namespace std;
 class Solution {
 public:
   double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
-    vector<int> nums;
+    vector<int>::iterator it1 = nums1.begin();
+    vector<int>::iterator it2 = nums2.begin();
 
-    vector<int>::iterator i1 = nums1.begin();
-    vector<int>::iterator i2 = nums2.begin();
+    vector<int>::iterator prev, curr;
 
-    int medianIndex = (nums1.size() + nums2.size()) / 2;
+    for (int i = 0; i < (nums1.size() + nums2.size()) / 2 + 1; ++i) {
+      prev = curr;
 
-    while (true) {
-      if (nums.size() > medianIndex)
-        break;
-
-      if (i1 != nums1.end() && i2 != nums2.end()) {
-        if (*i1 < *i2)
-          nums.push_back(*i1++);
-        else
-          nums.push_back(*i2++);
-      } else {
-        if (i1 != nums1.end())
-          nums.push_back(*i1++);
-        if (i2 != nums2.end())
-          nums.push_back(*i2++);
-      }
+      if (it2 == nums2.end() || (it1 != nums1.end() && *it1 < *it2))
+        curr = it1++;
+      else
+        curr = it2++;
     }
 
-    if ((nums1.size() + nums2.size()) & 1)
-      return *(nums.end() - 1);
+    if ((nums1.size() + nums2.size()) % 2 == 0)
+      return (float)(*prev + *curr) / 2;
     else
-      return double(*(nums.end() - 1) + *(nums.end() - 2)) / 2;
+      return *curr;
   }
 };
