5dc8fdf0c1fa927d8138434b3ba34a65439f25df
Givikap <kaitosekiya@outlook.com>
2025-12-23 00:06:53 -0600

    Add second faster Python solution for '6. Zigzag Conversion' problem

--- medium/0006_zigzag_conversion/solution.py
+++ medium/0006_zigzag_conversion/solution.py
@@ -3,16 +3,19 @@ class Solution:
         if num_rows == 1:
             return s
 
+        s_len = len(s)
+        step = 2 * num_rows - 2
+
         zigzag_s = []
 
         for row in range(num_rows):
-            for i in range(row, len(s), 2 * num_rows - 2):
+            for i in range(row, s_len, step):
                 zigzag_s.append(s[i])
 
                 if (
                     row > 0
                     and row < num_rows - 1
-                    and 2 * (num_rows - row - 1) + i < len(s)
+                    and 2 * (num_rows - row - 1) + i < s_len
                 ):
                     zigzag_s.append(s[2 * (num_rows - row - 1) + i])
 
