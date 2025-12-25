6901203ea605be2fc0703fd3b3117a144b45ca58
Givikap <kaitosekiya@outlook.com>
2025-12-25 15:53:33 -0600

    Add first Python solution for '1480. Running Sum of 1d Array' problem

---
+++ easy/1480_running_sum_of_1d_array/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running_sum = []
+        curr_sum = 0
+
+        for num in nums:
+            curr_sum += num
+            running_sum.append(curr_sum)
+
+        return running_sum
