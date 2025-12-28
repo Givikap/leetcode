1083781200cd62966d4e6ac92559e05f58756bd0
Givikap <kaitosekiya@outlook.com>
2025-12-28 02:00:04 -0600

    Add second faster Python solution for '3442. Maximum Difference Between Even and Odd Frequency I' problem

--- easy/3442_maximum_difference_between_even_and_odd_frequency_i/solution.py
+++ easy/3442_maximum_difference_between_even_and_odd_frequency_i/solution.py
@@ -3,15 +3,13 @@ from collections import Counter
 
 class Solution:
     def maxDifference(self, s: str) -> int:
-        s_counter = Counter(s)
-
-        max_odd_frequency = -1
+        max_odd_frequency = 0
         min_even_frequency = 101
 
-        for frequency in s_counter.values():
-            if frequency % 2 == 1 and frequency > max_odd_frequency:
-                max_odd_frequency = frequency
-            elif frequency % 2 == 0 and frequency < min_even_frequency:
-                min_even_frequency = frequency
+        for frequency in Counter(s).values():
+            if frequency % 2:
+                max_odd_frequency = max(frequency, max_odd_frequency)
+            else:
+                min_even_frequency = min(frequency, min_even_frequency)
 
         return max_odd_frequency - min_even_frequency
