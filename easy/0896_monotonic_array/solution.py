e7c605b1f874071352abb354af227dcfe650338e
Givikap <kaitosekiya@outlook.com>
2026-01-05 01:45:14 -0600

    Add second faster Python solution for '896. Monotonic Array' problem

--- easy/0896_monotonic_array/solution.py
+++ easy/0896_monotonic_array/solution.py
@@ -6,17 +6,16 @@ class Solution:
         if len(nums) == 1:
             return True
 
-        increasing = False
-        decreasing = False
+        increasing = True
+        decreasing = True
 
         for i in range(1, len(nums)):
-            if (increasing and nums[i] < nums[i - 1]) or (
-                decreasing and nums[i] > nums[i - 1]
-            ):
+            if nums[i] < nums[i - 1]:
+                increasing = False
+            if nums[i] > nums[i - 1]:
+                decreasing = False
+
+            if not increasing and not decreasing:
                 return False
-            elif nums[i] > nums[i - 1]:
-                increasing = True
-            elif nums[i] < nums[i - 1]:
-                decreasing = True
 
         return True
