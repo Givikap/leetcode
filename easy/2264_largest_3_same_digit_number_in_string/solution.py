563b36424bc696bdba3c03020ae27aaf30513387
Givikap <kaitosekiya@outlook.com>
2026-01-07 00:21:57 -0600

    Add first Python solution for '2264. Largest 3-Same-Digit Number in String' problem

---
+++ easy/2264_largest_3_same_digit_number_in_string/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        for n in (
+            "999",
+            "888",
+            "777",
+            "666",
+            "555",
+            "444",
+            "333",
+            "222",
+            "111",
+            "000",
+        ):
+            if n in num:
+                return n
+
+        return ""
