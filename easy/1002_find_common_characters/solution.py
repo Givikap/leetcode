a18c5926bcfb2d97eaf23824f46acfd49b5ebfd1
Givikap <kaitosekiya@outlook.com>
2026-01-17 01:11:57 -0600

    Add third faster Python solution for '1002. Find Common Characters' problem

--- easy/1002_find_common_characters/solution.py
+++ easy/1002_find_common_characters/solution.py
@@ -1,21 +1,11 @@
-from collections import Counter
 from typing import List
 
 
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        word_counters = [Counter(word) for word in words]
         common_chars = []
 
-        for c in set("".join(words)):
-            min_count = 101
-
-            for word_counter in word_counters:
-                min_count = min(min_count, word_counter[c])
-
-                if min_count == 0:
-                    break
-            else:
-                common_chars.extend([c] * min_count)
+        for c in set(words[0]):
+            common_chars.extend([c] * min(word.count(c) for word in words))
 
         return common_chars
