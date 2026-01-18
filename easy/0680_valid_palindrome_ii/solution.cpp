7c0f2a9b4dbb116a6b6297e93baa0d671872a1df
Givikap <kaitosekiya@outlook.com>
2026-01-18 03:27:57 -0600

    Add first C++ solution for '680. Valid Palindrome II' problem

---
+++ easy/0680_valid_palindrome_ii/solution.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+
+#include <string>
+
+class Solution {
+private:
+  bool isPalindrome(string &s, int left, int right) {
+    while (left < right) {
+      if (s[left++] != s[right--])
+        return false;
+    }
+
+    return true;
+  }
+
+public:
+  bool validPalindrome(string s) {
+    int left = 0;
+    int right = s.size() - 1;
+
+    while (left < right) {
+      if (s[left++] != s[right--])
+        return isPalindrome(s, left, right + 1) ||
+               isPalindrome(s, left - 1, right);
+    }
+
+    return true;
+  }
+};
