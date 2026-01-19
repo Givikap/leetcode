63669dcb686bd0aaf7009b3a9aa734290cb70c86
Givikap <kaitosekiya@outlook.com>
2026-01-19 02:18:50 -0600

    Add second faster Python solution for '1380. Lucky Numbers in a Matrix' problem

--- easy/1380_lucky_numbers_in_a_matrix/solution.py
+++ easy/1380_lucky_numbers_in_a_matrix/solution.py
@@ -4,22 +4,15 @@ from typing import List
 class Solution:
     def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
         rows = len(matrix)
-        cols = len(matrix[0])
-
-        min_num_by_row = [0] * rows
-        max_num_by_col = [0] * cols
-
-        for row in range(rows):
-            min_num_by_row[row] = min(matrix[row])
-
-        for col in range(cols):
-            max_num_by_col[col] = max(matrix[row][col] for row in range(rows))
 
         lucky_nums = []
 
         for row in range(rows):
-            for col in range(cols):
-                if min_num_by_row[row] == max_num_by_col[col]:
-                    lucky_nums.append(matrix[row][col])
+            row_min = min(matrix[row])
+            col = matrix[row].index(row_min)
+            col_max = max(matrix[row][col] for row in range(rows))
+
+            if row_min == col_max:
+                lucky_nums.append(row_min)
 
         return lucky_nums
