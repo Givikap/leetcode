31a4ac388d6fa2930e4ffda4d647fa6442833eb6
Givikap <kaitosekiya@outlook.com>
2026-01-21 00:28:47 -0600

    Add second faster C++ solution for '1636. Sort Array by Increasing Frequency' problem

--- easy/1636_sort_array_by_increasing_frequency/solution.cpp
+++ easy/1636_sort_array_by_increasing_frequency/solution.cpp
@@ -11,25 +11,13 @@ public:
     for (const int &num : nums)
       ++numsCounter[num];
 
-    vector<pair<int, int>> numsCounterVector(numsCounter.begin(),
-                                             numsCounter.end());
+    sort(nums.begin(), nums.end(), [&](const int &a, const int &b) {
+      if (numsCounter[a] != numsCounter[b])
+        return numsCounter[a] < numsCounter[b];
 
-    sort(numsCounterVector.begin(), numsCounterVector.end(),
-         [](const pair<int, int> &a, const pair<int, int> &b) {
-           if (a.second != b.second)
-             return a.second < b.second;
+      return a > b;
+    });
 
-           return a.first > b.first;
-         });
-
-    vector<int> sortedNums;
-    sortedNums.reserve(nums.size());
-
-    for (const auto &[num, count] : numsCounterVector) {
-      for (int _ = 0; _ < count; ++_)
-        sortedNums.push_back(num);
-    }
-
-    return sortedNums;
+    return nums;
   }
 };
