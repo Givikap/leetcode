13adb2ea73f13c47edde0fcd8d641f159be84b29
Givikap <kaitosekiya@outlook.com>
2026-01-22 00:55:37 -0600

    Add first C++ solution for '1260. Shift 2D Grid' problem

---
+++ easy/1260_shift_2d_grid/solution.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+  vector<vector<int>> shiftGrid(vector<vector<int>> &grid, int k) {
+    const int rows = grid.size();
+    const int cols = grid[0].size();
+
+    const int size = rows * cols;
+
+    vector<int> flattenGrid;
+
+    for (const vector<int> &row : grid) {
+      for (const int &num : row)
+        flattenGrid.push_back(num);
+    }
+
+    vector<int> flattenShiftedGrid(size, 0);
+
+    for (int i = 0; i < size; ++i)
+      flattenShiftedGrid[(i + k) % size] = flattenGrid[i];
+
+    vector<vector<int>> shiftedGrid(rows, vector<int>(cols, 0));
+
+    for (int row = 0; row < rows; ++row) {
+      for (int col = 0; col < cols; ++col) {
+        shiftedGrid[row][col] = flattenShiftedGrid[row * cols + col];
+      }
+    }
+
+    return shiftedGrid;
+  }
+};
