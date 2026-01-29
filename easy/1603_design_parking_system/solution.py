cba4f3a0670132daec759aacae1e6e99afbff706
Givikap <kaitosekiya@outlook.com>
2026-01-28 18:24:37 -0600

    Add second faster Python solution for '1603. Design Parking System' problem

--- easy/1603_design_parking_system/solution.py
+++ easy/1603_design_parking_system/solution.py
@@ -1,10 +1,10 @@
 class ParkingSystem:
     def __init__(self, big: int, medium: int, small: int):
-        self.__parking = {1: big, 2: medium, 3: small}
+        self.__parking = [big, medium, small]
 
     def addCar(self, car_type: int) -> bool:
-        if not self.__parking[car_type]:
+        if not self.__parking[car_type - 1]:
             return False
 
-        self.__parking[car_type] -= 1
+        self.__parking[car_type - 1] -= 1
         return True
