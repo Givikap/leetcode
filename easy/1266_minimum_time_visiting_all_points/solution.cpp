a0a311adf9aa0fbc4f3e88cfa645a0ba32cadd8f
Givikap <kaitosekiya@outlook.com>
2026-01-31 02:33:45 -0600

    Add second faster C++ solution for '1266. Minimum Time Visiting All Points' problem

--- easy/1266_minimum_time_visiting_all_points/solution.cpp
+++ easy/1266_minimum_time_visiting_all_points/solution.cpp
@@ -8,23 +8,18 @@ public:
     if (points.size() == 1)
       return 0;
 
+    const int pointsLen = points.size();
+
+    int x1, y1, x2, y2;
     int time = 0;
 
-    int x1 = points.back()[0];
-    int y1 = points.back()[1];
-    points.pop_back();
-
-    int x2, y2;
-
-    while (!points.empty()) {
-      x2 = points.back()[0];
-      y2 = points.back()[1];
-      points.pop_back();
+    for (int i = 0; i < pointsLen - 1; ++i) {
+      x1 = points[i][0];
+      y1 = points[i][1];
+      x2 = points[i + 1][0];
+      y2 = points[i + 1][1];
 
       time += max(abs(x1 - x2), abs(y1 - y2));
-
-      x1 = x2;
-      y1 = y2;
     }
 
     return time;
