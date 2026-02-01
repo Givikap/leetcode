2f943ada05bd318c1add2e9f3cc141acf3cf2c1e
Givikap <kaitosekiya@outlook.com>
2026-02-01 04:03:42 -0600

    Add first Python solution for '819. Most Common Word' problem

---
+++ easy/0819_most_common_word/solution.py
@@ -0,0 +1,30 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words_counter = Counter(
+            (
+                paragraph.translate(
+                    str.maketrans(
+                        {
+                            "!": " ",
+                            "?": " ",
+                            "'": " ",
+                            ",": " ",
+                            ";": " ",
+                            ".": " ",
+                        }
+                    )
+                )
+                .lower()
+                .split()
+            )
+        )
+
+        for word in banned:
+            if word in words_counter:
+                words_counter[word] = 0
+
+        return max((count, word) for word, count in words_counter.items())[1]
