748bb63fe08140bdaf03f4a3d3e3b5f9f3c5a80e
Givikap <kaitosekiya@outlook.com>
2026-02-04 01:15:14 -0600

    Add third faster Python solution for '455. Assign Cookies' problem

--- easy/0455_assign_cookies/solution.py
+++ easy/0455_assign_cookies/solution.py
@@ -10,13 +10,11 @@ class Solution:
         s_len = len(s)
         g_i = s_i = 0
 
-        cookies_count = 0
-
         while g_i < g_len and s_i < s_len:
             if g[g_i] <= s[s_i]:
                 g_i += 1
-                cookies_count += 1
+                s_i += 1
+            else:
+                s_i += 1
 
-            s_i += 1
-
-        return cookies_count
+        return g_i
