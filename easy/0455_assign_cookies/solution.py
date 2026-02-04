1520908de4bd0e3c397bbe2b1e4c50fe2f5534b8
Givikap <kaitosekiya@outlook.com>
2026-02-04 00:39:49 -0600

    Add second faster Python solution for '455. Assign Cookies' problem

--- easy/0455_assign_cookies/solution.py
+++ easy/0455_assign_cookies/solution.py
@@ -3,17 +3,20 @@ from typing import List
 
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        g.sort(reverse=True)
-        s.sort(reverse=True)
+        g.sort()
+        s.sort()
+
+        g_len = len(g)
+        s_len = len(s)
+        g_i = s_i = 0
 
         cookies_count = 0
 
-        while g and s:
-            if g[-1] <= s[-1]:
-                g.pop()
-                s.pop()
+        while g_i < g_len and s_i < s_len:
+            if g[g_i] <= s[s_i]:
+                g_i += 1
                 cookies_count += 1
-            else:
-                s.pop()
+
+            s_i += 1
 
         return cookies_count
