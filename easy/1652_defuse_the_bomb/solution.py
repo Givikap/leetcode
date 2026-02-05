5359fcc4780a3bb02b23eed9d783c76a50d7a1ed
Givikap <kaitosekiya@outlook.com>
2026-02-05 01:29:23 -0600

    Add second faster Python solution for '1652. Defuse the Bomb' problem

--- easy/1652_defuse_the_bomb/solution.py
+++ easy/1652_defuse_the_bomb/solution.py
@@ -11,22 +11,17 @@ class Solution:
         decrypted_code = []
 
         if k > 0:
-            next_k_sum = sum(code[i % code_len] for i in range(1, k + 1))
+            k_sum = sum(code[i] for i in range(1, k + 1))
             i = (k + 1) % code_len
-
-            for _ in range(code_len):
-                decrypted_code.append(next_k_sum)
-                next_k_sum += code[i % code_len] - code[(i - k) % code_len]
-                i = (i + 1) % code_len
         else:
-            prev_k_sum = sum(
-                code[i % code_len] for i in range(code_len + k, code_len)
-            )
+            k_sum = sum(code[i] for i in range(code_len + k, code_len))
             i = 0
 
-            for _ in range(code_len):
-                decrypted_code.append(prev_k_sum)
-                prev_k_sum += code[i % code_len] - code[(i + k) % code_len]
-                i = (i + 1) % code_len
+        k = abs(k)
+
+        for _ in range(code_len):
+            decrypted_code.append(k_sum)
+            k_sum += code[i] - code[i - k]
+            i = (i + 1) % code_len
 
         return decrypted_code
