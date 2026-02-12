66fcf1e21cb61724e49b5641edc25b0dcd7701f7
Givikap <kaitosekiya@outlook.com>
2026-02-11 21:22:05 -0600

    Add second faster C++ solution for '8. String to Integer (atoi)' problem

--- medium/0008_string_to_integer/solution.cpp
+++ medium/0008_string_to_integer/solution.cpp
@@ -8,39 +8,27 @@ public:
   int myAtoi(string s) {
     const int sLen = s.size();
 
-    vector<int> digits;
-    digits.reserve(sLen);
-
-    bool isPositive = true;
-
     int i = 0;
     while (i < sLen && s[i] == ' ')
       ++i;
 
-    if (s[i] == '-') {
-      isPositive = false;
+    int sign = 1;
+    if (s[i] == '+' || s[i] == '-') {
+      sign = s[i] == '-' ? -1 : 1;
       ++i;
-    } else if (s[i] == '+')
-      ++i;
-
-    for (; i < sLen; ++i) {
-      if (isdigit(s[i]))
-        digits.push_back(s[i] - '0');
-      else
-        break;
     }
 
     long long num = 0;
 
-    for (const int digit : digits) {
-      num = num * 10 + digit;
+    while (i < sLen && isdigit(s[i])) {
+      num = num * 10 + (s[i++] - '0');
 
-      if (isPositive && num > INT_MAX)
+      if (sign == 1 && num > INT_MAX)
         return INT_MAX;
-      if (!isPositive && -num < INT_MIN)
+      if (sign == -1 && -num < INT_MIN)
         return INT_MIN;
     }
 
-    return (int)(num * (isPositive ? 1 : -1));
+    return (int)(sign * num);
   }
 };
