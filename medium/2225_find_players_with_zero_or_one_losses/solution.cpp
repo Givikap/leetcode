9041a3d0a37a5037dd977ece0b30bce070ffd719
Givikap <kaitosekiya@outlook.com>
2026-02-15 00:49:27 -0600

    Add second faster C++ solution for '2225. Find Players With Zero or One Losses' problem

--- medium/2225_find_players_with_zero_or_one_losses/solution.cpp
+++ medium/2225_find_players_with_zero_or_one_losses/solution.cpp
@@ -7,32 +7,22 @@ using namespace std;
 class Solution {
 public:
   vector<vector<int>> findWinners(vector<vector<int>> &matches) {
-    set<int> winners;
-    set<int> oneLoss;
-    set<int> manyLosses;
+    unordered_map<int, int> lossCounter;
 
     for (const vector<int> &match : matches) {
-      int winner = match[0];
-      int loser = match[1];
-
-      winners.insert(winner);
-
-      if (oneLoss.find(loser) == oneLoss.end())
-        oneLoss.insert(loser);
-      else
-        manyLosses.insert(loser);
+      lossCounter[match[0]];
+      ++lossCounter[match[1]];
     }
 
     vector<vector<int>> answer(2);
-    set<int> temp;
 
-    set_difference(winners.begin(), winners.end(), oneLoss.begin(),
-                   oneLoss.end(), inserter(temp, temp.begin()));
-    set_difference(temp.begin(), temp.end(), manyLosses.begin(),
-                   manyLosses.end(), back_inserter(answer[0]));
+    for (const auto &[loser, count] : lossCounter) {
+      if (count < 2)
+        answer[count].push_back(loser);
+    }
 
-    set_difference(oneLoss.begin(), oneLoss.end(), manyLosses.begin(),
-                   manyLosses.end(), back_inserter(answer[1]));
+    sort(answer[0].begin(), answer[0].end());
+    sort(answer[1].begin(), answer[1].end());
 
     return answer;
   }
