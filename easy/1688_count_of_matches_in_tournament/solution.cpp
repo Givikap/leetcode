5779e336dfd6e402f25ad4cd0fc4883680726776
Givikap <kaitosekiya@outlook.com>
2026-02-16 23:27:29 -0600

    Add second faster C++ solution for '1688. Count of Matches in Tournament' problem

--- easy/1688_count_of_matches_in_tournament/solution.cpp
+++ easy/1688_count_of_matches_in_tournament/solution.cpp
@@ -2,18 +2,5 @@ using namespace std;
 
 class Solution {
 public:
-  int numberOfMatches(int n) {
-    int matchesCount = 0;
-
-    while (n > 1) {
-      matchesCount += n / 2;
-
-      if (n % 2 == 1)
-        ++n;
-
-      n /= 2;
-    }
-
-    return matchesCount;
-  }
+  int numberOfMatches(int n) { return n - 1; }
 };
