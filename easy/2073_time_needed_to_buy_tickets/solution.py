13a6746c89d7e43ef9a63af12a585c14f8790d24
Givikap <kaitosekiya@outlook.com>
2026-02-18 00:38:41 -0600

    Add second faster Python solution for '2073. Time Needed to Buy Tickets' problem

--- easy/2073_time_needed_to_buy_tickets/solution.py
+++ easy/2073_time_needed_to_buy_tickets/solution.py
@@ -1,21 +1,8 @@
-from collections import deque
 from typing import List
 
 
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        queue = deque((i, ticket) for i, ticket in enumerate(tickets))
-        time = 0
-
-        while queue:
-            i, ticket = queue.popleft()
-
-            time += 1
-            ticket -= 1
-
-            if ticket > 0:
-                queue.append((i, ticket))
-            elif i == k:
-                break
-
-        return time
+        return sum(
+            min(tickets[i], tickets[k] - (i > k)) for i in range(len(tickets))
+        )
