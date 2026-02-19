bd151dffa9e997e26b102339b257b868e8c98b1d
Givikap <kaitosekiya@outlook.com>
2026-02-19 00:26:33 -0600

    Add first C++ solution for '1752. Check if Array Is Sorted and Rotated' problem

---
+++ easy/1752_check_if_array_is_sorted_and_rotated/solution.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+  bool check(vector<int> &nums) {
+    const int numsLen = nums.size();
+
+    int i;
+    for (i = 1; i < numsLen && nums[i - 1] <= nums[i]; ++i) {
+    }
+
+    if (i == numsLen)
+      return true;
+
+    for (; i < numsLen - 1; ++i)
+      if (nums[i] > nums[i + 1])
+        return false;
+
+    return nums[0] >= nums[numsLen - 1];
+  }
+};
