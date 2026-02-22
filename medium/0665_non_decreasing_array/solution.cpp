61f82243b02932d1e39b5d63e6b90d62f749c20f
Givikap <kaitosekiya@outlook.com>
2026-02-22 02:10:05 -0600

    Add first C++ solution for '665. Non-decreasing Array' problem

---
+++ medium/0665_non_decreasing_array/solution.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+  bool checkPossibility(vector<int> &nums) {
+    bool modified = false;
+
+    for (int i = 0; i + 1 < nums.size(); ++i) {
+      if (nums[i] > nums[i + 1]) {
+        if (modified)
+          return false;
+        else
+          modified = true;
+
+        if (i > 0 && nums[i - 1] > nums[i + 1])
+          nums[i + 1] = nums[i];
+        else
+          nums[i] = nums[i + 1];
+      }
+    }
+
+    return true;
+  }
+};
