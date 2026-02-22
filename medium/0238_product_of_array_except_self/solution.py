2bc08378f8abbe84624ad430c330db81d73a633a
Givikap <kaitosekiya@outlook.com>
2026-02-21 19:21:51 -0600

    Add second faster Python solution for '238. Product of Array Except Self' problem

--- medium/0238_product_of_array_except_self/solution.py
+++ medium/0238_product_of_array_except_self/solution.py
@@ -5,13 +5,16 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         nums_len = len(nums)
 
-        prefix_sums = [1, nums[0]]
-        postfix_sums = [nums[-1]] * (nums_len - 1) + [1]
+        num_products = [1] * nums_len
 
-        for i in range(1, nums_len - 1):
-            prefix_sums.append(prefix_sums[i] * nums[i])
+        prefix = 1
+        for i in range(nums_len):
+            num_products[i] = prefix
+            prefix *= nums[i]
 
-        for i in range(nums_len - 2, 0, -1):
-            postfix_sums[i - 1] = postfix_sums[i] * nums[i]
+        suffix = 1
+        for i in range(nums_len - 1, -1, -1):
+            num_products[i] *= suffix
+            suffix *= nums[i]
 
-        return [prefix_sums[i] * postfix_sums[i] for i in range(nums_len)]
+        return num_products
