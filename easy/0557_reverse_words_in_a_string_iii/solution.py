d0e7bb29f672e1cd67b97798bdce345dfc5b7897
Givikap <kaitosekiya@outlook.com>
2026-02-23 03:03:52 -0600

    Add second faster Python solution for '557. Reverse Words in a String III' problem

--- easy/0557_reverse_words_in_a_string_iii/solution.py
+++ easy/0557_reverse_words_in_a_string_iii/solution.py
@@ -1,26 +1,3 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s_list = list(s)
-
-        i = s.find(" ")
-        left = 0
-
-        while i != -1:
-            right = i - 1
-
-            while left < right:
-                s_list[left], s_list[right] = s_list[right], s_list[left]
-                left += 1
-                right -= 1
-
-            left = i + 1
-            i = s.find(" ", i + 1)
-
-        right = len(s) - 1
-
-        while left < right:
-            s_list[left], s_list[right] = s_list[right], s_list[left]
-            left += 1
-            right -= 1
-
-        return "".join(s_list)
+        return " ".join(word[::-1] for word in s.split(" "))
