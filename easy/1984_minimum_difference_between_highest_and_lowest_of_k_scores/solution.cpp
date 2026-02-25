27cafd9a6fe483bcbf345c6919efabae66e23fb5
Givikap <kaitosekiya@outlook.com>
2026-02-25 00:20:07 -0600

    Add first C++ solution for '1984. Minimum Difference Between Highest and Lowest of K Scores' problem

---
+++ easy/1984_minimum_difference_between_highest_and_lowest_of_k_scores/solution.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+  int minimumDifference(vector<int> &nums, int k) {
+    sort(nums.begin(), nums.end());
+
+    int minDiff = 100000;
+
+    for (int i = 0; i < nums.size() - k + 1; ++i)
+      minDiff = min(minDiff, nums[i + k - 1] - nums[i]);
+
+    return minDiff;
+  }
+};
