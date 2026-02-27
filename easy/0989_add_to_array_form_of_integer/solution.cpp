b38e6a168542e97bb5d04870ecdf8002b3307264
Givikap <kaitosekiya@outlook.com>
2026-02-27 10:51:49 -0600

    Add first C++ solution for '989. Add to Array-Form of Integer' problem

---
+++ easy/0989_add_to_array_form_of_integer/solution.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+  vector<int> addToArrayForm(vector<int> &num, int k) {
+    int i = num.size() - 1;
+
+    int carry = 0;
+
+    while (k || carry) {
+      if (i == -1) {
+        num.push_back(-1);
+
+        for (int j = num.size() - 1; j > 0; --j)
+          num[j] = num[j - 1];
+
+        num[0] = 0;
+        i = 0;
+      }
+
+      num[i] += k % 10 + carry;
+      carry = num[i] / 10;
+      num[i] %= 10;
+
+      k /= 10;
+      --i;
+    }
+
+    return num;
+  }
+};
