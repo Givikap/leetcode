0e85c80fda39fd12f2e2333c917c41bd56f3b493
Givikap <kaitosekiya@outlook.com>
2026-03-01 16:25:11 -0600

    Add first C++ solution for '55. Jump Game' problem

---
+++ medium/0055_jump_game/solution.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+  bool canJump(vector<int> &nums) {
+    int maxI = 0;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      if (i > maxI)
+        return false;
+      maxI = max(maxI, i + nums[i]);
+    }
+
+    return true;
+  }
+};
