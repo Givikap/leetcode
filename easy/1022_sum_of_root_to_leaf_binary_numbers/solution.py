8bd5af22b5b64a543b4a25948d7b4110ad66e3fa
Givikap <kaitosekiya@outlook.com>
2026-03-03 00:51:57 -0600

    Add second faster Python solution for '1022. Sum of Root To Leaf Binary Numbers' problem

--- easy/1022_sum_of_root_to_leaf_binary_numbers/solution.py
+++ easy/1022_sum_of_root_to_leaf_binary_numbers/solution.py
@@ -5,29 +5,19 @@ from utils.python.nodes import TreeNode
 
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        stack = [(root, [root.val])]
-        bin_nums = []
+        stack = [(root, root.val)]
+        nums = []
 
         while stack:
-            node, bin_num = stack.pop()
+            node, num = stack.pop()
 
             if not node.left and not node.right:
-                bin_nums.append(bin_num)
+                nums.append(num)
                 continue
 
             if node.left:
-                stack.append((node.left, bin_num + [node.left.val]))
+                stack.append((node.left, (num << 1) | node.left.val))
             if node.right:
-                stack.append((node.right, bin_num + [node.right.val]))
+                stack.append((node.right, (num << 1) | node.right.val))
 
-        nums_sum = 0
-
-        for bin_num in bin_nums:
-            num = 0
-
-            for bit in bin_num:
-                num = (num << 1) | bit
-
-            nums_sum += num
-
-        return nums_sum
+        return sum(nums)
