d51a5c94281bcbbf77da946c1873d6e95d642ddb
Givikap <kaitosekiya@outlook.com>
2026-03-05 00:41:20 -0600

    Add second faster Python solution for '1582. Special Positions in a Binary Matrix' problem

--- easy/1582_special_positions_in_a_binary_matrix/solution.py
+++ easy/1582_special_positions_in_a_binary_matrix/solution.py
@@ -6,20 +6,21 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
 
-        row_sums = []
+        row_sums = [0] * rows
         col_sums = [0] * cols
 
         for row in range(rows):
-            row_sums.append(sum(matrix[row]))
-
             for col in range(cols):
-                col_sums[col] += matrix[row][col]
+                if matrix[row][col] == 1:
+                    row_sums[row] += matrix[row][col]
+                    col_sums[col] += matrix[row][col]
 
         count = 0
 
         for row in range(rows):
-            for col in range(cols):
-                if matrix[row][col] == row_sums[row] == col_sums[col] == 1:
-                    count += 1
+            if row_sums[row] == 1:
+                for col in range(cols):
+                    if col_sums[col] == 1 and matrix[row][col] == 1:
+                        count += 1
 
         return count
