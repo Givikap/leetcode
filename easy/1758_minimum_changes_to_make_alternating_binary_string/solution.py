62aebd8cb94917feb937a4726b238919164becbb
Givikap <kaitosekiya@outlook.com>
2026-03-07 01:57:09 -0600

    Add second faster Python solution for '1758. Minimum Changes To Make Alternating Binary String' problem

--- easy/1758_minimum_changes_to_make_alternating_binary_string/solution.py
+++ easy/1758_minimum_changes_to_make_alternating_binary_string/solution.py
@@ -5,15 +5,13 @@ class Solution:
         if len(s) == 2:
             return int(s[0] == s[1])
 
-        zeros = ones = 0
-        zero = True
+        changes_count = 0
+        zero = False
 
         for i in range(len(s)):
-            if (zero and s[i] != "0") or (not zero and s[i] != "1"):
-                zeros += 1
-            if (zero and s[i] != "1") or (not zero and s[i] != "0"):
-                ones += 1
+            if (not zero and s[i] != "0") or (zero and s[i] != "1"):
+                changes_count += 1
 
             zero = not zero
 
-        return min(zeros, ones)
+        return min(changes_count, len(s) - changes_count)
