7d5c050576675d0ba6099699e74a614334e82789
Givikap <kaitosekiya@outlook.com>
2026-03-12 21:12:15 -0500

    Add Union-Find implementation to C++ utils

---
+++ utils/cpp/union_find.hpp
@@ -0,0 +1,38 @@
+using namespace std;
+
+#include <vector>
+
+class UnionFind {
+private:
+  vector<int> parent;
+  vector<int> rank;
+
+public:
+  UnionFind(int n) : parent(n), rank(n) {
+    for (int i = 0; i < n; ++i)
+      parent[i] = i;
+  }
+
+  int find(int x) {
+    if (parent[x] != x)
+      parent[x] = find(parent[x]);
+
+    return parent[x];
+  }
+
+  void merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+
+    if (x == y)
+      return;
+
+    if (rank[x] < rank[y]) {
+      parent[x] = y;
+      rank[y] += rank[x];
+    } else {
+      parent[y] = x;
+      rank[x] += rank[y];
+    }
+  }
+};
