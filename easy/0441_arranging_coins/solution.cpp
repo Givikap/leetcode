cd62173f68e7b3279f8a11807c3aa97afa2da1ee
Givikap <kaitosekiya@outlook.com>
2026-03-15 00:52:26 -0500

    Add second faster C++ solution for '441. Arranging Coins' problem

--- easy/0441_arranging_coins/solution.cpp
+++ easy/0441_arranging_coins/solution.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
   int arrangeCoins(int n) {
-    int row = 1;
+    long left = 1;
+    long right = n;
 
-    while (n >= row)
-      n -= row++;
+    while (left <= right) {
+      long mid = left + (right - left) / 2;
+      long coins = mid * (mid + 1) / 2;
 
-    return row - 1;
+      if (coins == n)
+        return mid;
+
+      if (coins < n)
+        left = mid + 1;
+      else
+        right = mid - 1;
+    }
+
+    return right;
   }
 };
