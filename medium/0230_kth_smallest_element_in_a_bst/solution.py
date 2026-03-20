b651c3a47b1a3f78debb3d3504cfebefb534b4b9
Givikap <kaitosekiya@outlook.com>
2026-03-19 21:30:30 -0500

    Add second faster Python solution for '230. Kth Smallest Element in a BST' problem

--- medium/0230_kth_smallest_element_in_a_bst/solution.py
+++ medium/0230_kth_smallest_element_in_a_bst/solution.py
@@ -1,4 +1,3 @@
-import heapq
 from typing import Optional
 
 from utils.python.nodes import TreeNode
@@ -6,16 +5,18 @@ from utils.python.nodes import TreeNode
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        stack = [root]
+        curr = root
+        stack = []
+
         values = []
 
-        while stack:
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+
             node = stack.pop()
             values.append(node.val)
+            curr = node.right
 
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-
-        return heapq.nsmallest(k, values)[-1]
+        return values[k - 1]
