06880a6244e4925c954af16d4bfb2e03b7fc71ea
Givikap <kaitosekiya@outlook.com>
2026-03-27 02:04:34 -0500

    Add second faster Python solution for '3833. Count Dominant Indices' problem

--- easy/3833_count_dominant_indices/solution.py
+++ easy/3833_count_dominant_indices/solution.py
@@ -3,15 +3,16 @@ from typing import List
 
 class Solution:
     def dominantIndices(self, nums: List[int]) -> int:
-        postfix_sums = [0] * len(nums)
-
-        for i in range(len(nums) - 2, -1, -1):
-            postfix_sums[i] = postfix_sums[i + 1] + nums[i + 1]
+        postfix_sum = nums[-1]
+        postfix_count = 1
 
         count = 0
 
-        for i in range(len(nums) - 1):
-            if nums[i] > postfix_sums[i] / (len(nums) - i - 1):
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] * postfix_count > postfix_sum:
                 count += 1
+
+            postfix_sum += nums[i]
+            postfix_count += 1
 
         return count
