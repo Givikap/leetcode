ef2ddb776dc695b2914c91e00d755ef8a602ad3c
Givikap <kaitosekiya@outlook.com>
2026-03-28 01:17:50 -0500

    Add second faster Python solution for '3838. Weighted Word Mapping' problem

--- easy/3838_weighted_word_mapping/solution.py
+++ easy/3838_weighted_word_mapping/solution.py
@@ -3,12 +3,13 @@ from typing import List
 
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        letters = [0] * len(words)
+        letters = []
 
-        for i, word in enumerate(words):
+        for word in words:
+            val = 0
             for c in word:
-                letters[i] += weights[ord(c) - 97]
+                val += weights[ord(c) - 97]
 
-            letters[i] = chr(122 - (letters[i] % 26))
+            letters.append(chr(122 - (val % 26)))
 
         return "".join(letters)
