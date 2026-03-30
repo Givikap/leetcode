1babe80864195a6a4327cb0e6d3e21e71f11e30e
Givikap <kaitosekiya@outlook.com>
2026-03-30 10:23:20 -0500

    Add second faster C++ solution for '3492. Maximum Containers on a Ship' problem

--- easy/3492_maximum_containers_on_a_ship/solution.cpp
+++ easy/3492_maximum_containers_on_a_ship/solution.cpp
@@ -1,18 +1,8 @@
+#include <algorithm>
+
 class Solution {
 public:
   int maxContainers(int n, int w, int maxWeight) {
-    int weight = 0;
-
-    int containers;
-    const int maxContainers = n * n;
-
-    for (containers = 0; containers < maxContainers; ++containers) {
-      if (weight + w > maxWeight)
-        return containers;
-
-      weight += w;
-    }
-
-    return containers;
+    return std::min(n * n, maxWeight / w);
   }
 };
