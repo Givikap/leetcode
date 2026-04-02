df9161cdb69ce39e9de4e6c51780644310f773f1
Givikap <kaitosekiya@outlook.com>
2026-04-01 23:58:04 -0500

    Add third faster Python solution for '2906. Construct Product Matrix' problem

--- medium/2906_construct_product_matrix/solution.py
+++ medium/2906_construct_product_matrix/solution.py
@@ -8,16 +8,18 @@ class Solution:
 
         product_grid = [[1] * cols for _ in range(rows)]
 
-        matrix_product = 1
-
+        prefix_product = 1
         for row in range(rows):
             for col in range(cols):
-                matrix_product *= grid[row][col]
+                product_grid[row][col] = prefix_product
+                prefix_product = (prefix_product * grid[row][col]) % 12345
 
-        for row in range(rows):
-            for col in range(cols):
+        suffix_product = 1
+        for row in range(rows - 1, -1, -1):
+            for col in range(cols - 1, -1, -1):
                 product_grid[row][col] = (
-                    matrix_product // grid[row][col]
+                    product_grid[row][col] * suffix_product
                 ) % 12345
+                suffix_product = (suffix_product * grid[row][col]) % 12345
 
         return product_grid
