0be85f7c409c2fc61411ffe5d8514548fbb977e5
Givikap <kaitosekiya@outlook.com>
2026-04-03 01:44:02 -0500

    Add second faster Python solution for '492. Construct the Rectangle' problem

--- easy/0492_construct_the_rectangle/solution.py
+++ easy/0492_construct_the_rectangle/solution.py
@@ -4,10 +4,8 @@ from typing import List
 
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
-        rectangle = [area, 1]
-
-        for side in range(2, int(math.sqrt(area)) + 1):
+        for side in range(int(math.sqrt(area)), 0, -1):
             if area % side == 0:
-                rectangle = [area // side, side]
+                return [area // side, side]
 
-        return rectangle
+        return [area, 1]
