949b77e4b4b07a5da28436fa2084b982c0141722
Givikap <kaitosekiya@outlook.com>
2026-04-05 02:04:50 -0500

    Add first C++ solution for '598. Range Addition II' problem

---
+++ easy/0598_range_addition_ii/solution.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+class Solution {
+public:
+  int maxCount(int m, int n, std::vector<std::vector<int>> &ops) {
+    for (const std::vector<int> &op : ops) {
+      m = std::min(m, op[0]);
+      n = std::min(n, op[1]);
+    }
+
+    return m * n;
+  }
+};
