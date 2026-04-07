908ab4d326f19d8ac37df8b493cf7a2834f9fba1
Givikap <kaitosekiya@outlook.com>
2026-04-07 01:39:18 -0500

    Add second faster Python solution for '2069. Walking Robot Simulation II' problem

--- medium/2069_walking_robot_simulation_ii/solution.py
+++ medium/2069_walking_robot_simulation_ii/solution.py
@@ -3,6 +3,12 @@ from typing import List
 
 class Robot:
     directions = {0: "North", 90: "East", 180: "South", 270: "West"}
+    deltas = {
+        0: (0, 1),
+        90: (1, 0),
+        180: (0, -1),
+        270: (-1, 0),
+    }
 
     def __init__(self, width: int, height: int):
         self.width = width
@@ -13,33 +19,30 @@ class Robot:
         self.direction = 90
 
     def step(self, num: int) -> None:
-        if self.direction == 0:
-            self.y += num
-        elif self.direction == 90:
-            self.x += num
-        elif self.direction == 180:
-            self.y -= num
-        elif self.direction == 270:
-            self.x -= num
+        while True:
+            dx, dy = self.deltas[self.direction]
+            self.x += dx * num
+            self.y += dy * num
 
-        if self.x < 0:
-            num = abs(self.x)
-            self.x = 0
-        elif self.x >= self.width:
-            num = self.x - (self.width - 1)
-            self.x -= num
-        elif self.y < 0:
-            num = abs(self.y)
-            self.y = 0
-        elif self.y >= self.height:
-            num = self.y - (self.height - 1)
-            self.y -= num
-        else:
             num = 0
 
-        if num:
+            if self.x < 0:
+                num = -self.x
+                self.x = 0
+            elif self.x >= self.width:
+                num = self.x - (self.width - 1)
+                self.x = self.width - 1
+            elif self.y < 0:
+                num = -self.y
+                self.y = 0
+            elif self.y >= self.height:
+                num = self.y - (self.height - 1)
+                self.y = self.height - 1
+
+            if num == 0:
+                break
+
             self.direction = (self.direction - 90) % 360
-            self.step(num)
 
     def getPos(self) -> List[int]:
         return [self.x, self.y]
