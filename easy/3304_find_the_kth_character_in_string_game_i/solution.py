ed87586aeaa97319df022632a49db11c06794e9d
Givikap <kaitosekiya@outlook.com>
2026-04-09 23:59:57 -0500

    Add first Python solution for '3304. Find the K-th Character in String Game I' problem

---
+++ easy/3304_find_the_kth_character_in_string_game_i/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        word = "a"
+
+        while len(word) < k:
+            word += "".join(chr(ord(c) + 1) for c in word)
+
+        return word[k - 1]
