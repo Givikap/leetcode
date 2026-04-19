1a1e68e6aa7f32d979daa37b51735813e4fd4097
Givikap <kaitosekiya@outlook.com>
2026-04-19 02:36:00 -0500

    Add second faster Python solution for '459. Repeated Substring Pattern' problem

--- easy/0459_repeated_substring_pattern/solution.py
+++ easy/0459_repeated_substring_pattern/solution.py
@@ -1,10 +1,3 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        for end in range(1, len(s)):
-            if len(s) % end != 0:
-                continue
-
-            if s[:end] * (len(s) // end) == s:
-                return True
-
-        return False
+        return s in (s + s)[1:-1]
