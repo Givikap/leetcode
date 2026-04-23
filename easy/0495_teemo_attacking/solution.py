f83923d2b71e52b172dbe9b123f37d090e762ca8
Givikap <kaitosekiya@outlook.com>
2026-04-22 19:19:44 -0500

    Add second nicer Python solution for '495. Teemo Attacking' problem

--- easy/0495_teemo_attacking/solution.py
+++ easy/0495_teemo_attacking/solution.py
@@ -7,11 +7,7 @@ class Solution:
     ) -> int:
         time_series.append(time_series[-1] + duration)
 
-        poisoned_duration = 0
-
-        for i in range(1, len(time_series)):
-            poisoned_duration += min(
-                duration, time_series[i] - time_series[i - 1]
-            )
-
-        return poisoned_duration
+        return sum(
+            min(duration, time_series[i] - time_series[i - 1])
+            for i in range(1, len(time_series))
+        )
