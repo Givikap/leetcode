dfa6396c652c950562f4be9cf7368fa16f767f51
Givikap <kaitosekiya@outlook.com>
2026-04-23 01:52:24 -0500

    Add first Python solution for '501. Find Mode in Binary Search Tree' problem

---
+++ easy/0501_find_mode_in_binary_search_tree/solution.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from typing import List, Optional
+
+from utils.python.nodes import TreeNode
+
+
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        vals_counter = Counter()
+
+        stack = [root]
+        while stack:
+            node = stack.pop()
+
+            vals_counter[node.val] += 1
+
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return [
+            val
+            for val, count in vals_counter.items()
+            if count == max(vals_counter.values())
+        ]
