a83e0b87b6bdcc1f3626f9c22ffeff20bbe506be
Givikap <kaitosekiya@outlook.com>
2026-04-24 00:08:57 -0500

    Add second faster Python solution for '2833. Furthest Point From Origin' problem

--- easy/2833_furthest_point_from_origin/solution.py
+++ easy/2833_furthest_point_from_origin/solution.py
@@ -1,9 +1,13 @@
-from collections import Counter
-
-
 class Solution:
     def furthestDistanceFromOrigin(self, moves: str) -> int:
-        moves_counter = Counter(moves)
-        return (
-            abs(moves_counter["L"] - moves_counter["R"]) + moves_counter["_"]
-        )
+        left_count = right_count = blank_count = 0
+
+        for move in moves:
+            if move == "L":
+                left_count += 1
+            elif move == "R":
+                right_count += 1
+            else:
+                blank_count += 1
+
+        return abs(left_count - right_count) + blank_count
