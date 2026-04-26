56caf2dfa5099bc913203b5cbbdec0ff8495d17b
Givikap <kaitosekiya@outlook.com>
2026-04-26 02:49:10 -0500

    Add second faster Python solution for '18. 4Sum' problem

--- medium/0018_4sum/solution.py
+++ medium/0018_4sum/solution.py
@@ -4,11 +4,21 @@ from typing import List
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
+        nums_len = len(nums)
 
         four_sums = []
 
-        for outer_left in range(len(nums) - 3):
-            for outer_right in range(len(nums) - 1, outer_left + 2, -1):
+        for outer_left in range(nums_len - 3):
+            if outer_left > 0 and nums[outer_left] == nums[outer_left - 1]:
+                continue
+
+            for outer_right in range(nums_len - 1, outer_left + 2, -1):
+                if (
+                    outer_right < nums_len - 1
+                    and nums[outer_right] == nums[outer_right + 1]
+                ):
+                    continue
+
                 inner_left = outer_left + 1
                 inner_right = outer_right - 1
 
@@ -30,12 +40,22 @@ class Solution:
                             )
                         )
 
-                    if curr_sum < target:
+                        while (
+                            inner_left < inner_right
+                            and nums[inner_left] == nums[inner_left + 1]
+                        ):
+                            inner_left += 1
+                        while (
+                            inner_left < inner_right
+                            and nums[inner_right] == nums[inner_right - 1]
+                        ):
+                            inner_right -= 1
+
                         inner_left += 1
-                    elif curr_sum > target:
                         inner_right -= 1
-                    else:
+                    elif curr_sum < target:
                         inner_left += 1
+                    else:
                         inner_right -= 1
 
-        return list(map(list, set(four_sums)))
+        return four_sums
