3656b021665030b64c247e7463d8edfd3a89834f
Givikap <kaitosekiya@outlook.com>
2026-04-28 02:43:55 -0500

    Add first Java solution for '700. Search in a Binary Search Tree' problem

---
+++ easy/0700_search_in_a_binary_search_tree/Solution.java
@@ -0,0 +1,21 @@
+import utils.java.nodes.TreeNode;
+
+class Solution {
+  public TreeNode searchBST(TreeNode root, int val) {
+    TreeNode subroot = null;
+    TreeNode curr = root;
+
+    while (curr != null) {
+      if (curr.val == val) {
+        subroot = curr;
+        break;
+      } else if (curr.val < val) {
+        curr = curr.right;
+      } else {
+        curr = curr.left;
+      }
+    }
+
+    return subroot;
+  }
+}
