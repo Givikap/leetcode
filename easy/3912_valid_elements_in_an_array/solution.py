3335cfdf7365006aa21276119bbdcc4e7765d1e9
Givikap <kaitosekiya@outlook.com>
2026-04-30 23:20:24 -0500

    Add second faster Python solution for '3912. Valid Elements in an Array' problem

--- easy/3912_valid_elements_in_an_array/solution.py
+++ easy/3912_valid_elements_in_an_array/solution.py
@@ -3,21 +3,22 @@ from typing import List
 
 class Solution:
     def findValidElements(self, nums: List[int]) -> List[int]:
-        prefix_maxes = [0] * len(nums)
-        suffix_maxes = [0] * len(nums)
+        nums_len = len(nums)
 
-        for i in range(1, len(nums)):
+        prefix_maxes = [0] * nums_len
+        suffix_maxes = [0] * nums_len
+
+        for i, j in zip(range(1, nums_len), range(nums_len - 2, -1, -1)):
             prefix_maxes[i] = max(prefix_maxes[i - 1], nums[i - 1])
-        for i in range(len(nums) - 2, -1, -1):
-            suffix_maxes[i] = max(suffix_maxes[i + 1], nums[i + 1])
+            suffix_maxes[j] = max(suffix_maxes[j + 1], nums[j + 1])
 
         valid_nums = [nums[0]]
 
-        for i in range(1, len(nums) - 1):
+        for i in range(1, nums_len - 1):
             if nums[i] > prefix_maxes[i] or nums[i] > suffix_maxes[i]:
                 valid_nums.append(nums[i])
 
-        if len(nums) != 1:
+        if nums_len != 1:
             valid_nums.append(nums[-1])
 
         return valid_nums
