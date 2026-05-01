1ea9712080545fe4dcc57e02961db0b8cf499c46
Givikap <kaitosekiya@outlook.com>
2026-05-01 00:17:46 -0500

    Add second faster Python solution for '3908. Valid Digit Number' problem

--- easy/3908_valid_digit_number/solution.py
+++ easy/3908_valid_digit_number/solution.py
@@ -2,13 +2,13 @@ class Solution:
     def validDigit(self, n: int, x: int) -> bool:
         valid = False
 
-        while n:
-            if n == x:
-                valid = False
-                break
+        while n > 9:
             if n % 10 == x:
                 valid = True
 
             n //= 10
+
+        if n == x:
+            return False
 
         return valid
