113a106da23f8fc7b3065400166f4d30333ba64f
Givikap <kaitosekiya@outlook.com>
2026-05-02 02:42:41 -0500

    Add first C++ solution for '3894. Traffic Signal Color' problem

---
+++ easy/3894_traffic_signal_color/solution.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+class Solution {
+public:
+  std::string trafficSignal(int timer) {
+    if (timer == 0)
+      return "Green";
+    else if (timer == 30)
+      return "Orange";
+    else if (30 < timer && timer <= 90)
+      return "Red";
+    else
+      return "Invalid";
+  }
+};
