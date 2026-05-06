a487a444e4e5d14202c24c841ad77e75bb8ac077
Givikap <kaitosekiya@outlook.com>
2026-05-06 00:13:47 -0500

    Add second faster Java solution for '1903. Largest Odd Number in String' problem

--- easy/1903_largest_odd_number_in_string/Solution.java
+++ easy/1903_largest_odd_number_in_string/Solution.java
@@ -1,13 +1,11 @@
 class Solution {
   public String largestOddNumber(String num) {
-    int lastOdd = 0;
-
-    for (int i = 0; i < num.length(); ++i) {
+    for (int i = num.length() - 1; i > -1; --i) {
       if ((num.charAt(i) - 48) % 2 == 1) {
-        lastOdd = i + 1;
+        return num.substring(0, i + 1);
       }
     }
 
-    return num.substring(0, lastOdd);
+    return "";
   }
 }
