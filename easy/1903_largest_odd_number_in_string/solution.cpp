b2a8e2156240b0d89d05831a2a5c4707c5d6db9f
Givikap <kaitosekiya@outlook.com>
2026-05-06 00:15:17 -0500

    Add second faster C++ solution for '1903. Largest Odd Number in String' problem

--- easy/1903_largest_odd_number_in_string/solution.cpp
+++ easy/1903_largest_odd_number_in_string/solution.cpp
@@ -3,13 +3,11 @@
 class Solution {
 public:
   std::string largestOddNumber(std::string num) {
-    size_t lastOdd = 0;
-
-    for (size_t i = 0; i < num.size(); ++i) {
+    for (int i = num.size() - 1; i > -1; --i) {
       if ((num[i] - 48) % 2 == 1)
-        lastOdd = i + 1;
+        return num.substr(0, i + 1);
     }
 
-    return num.substr(0, lastOdd);
+    return "";
   }
 };
