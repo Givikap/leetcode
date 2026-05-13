e6a6846aa39e73551b124e735f5b16fe1cc13d59
Givikap <kaitosekiya@outlook.com>
2026-05-13 00:06:00 -0500

    Add second more optimized Python solution for '796. Rotate String' problem

--- easy/0796_rotate_string/solution.py
+++ easy/0796_rotate_string/solution.py
@@ -1,5 +1,8 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+
         for start in range(len(s)):
             if s[start] == goal[0]:
                 if s[start:] + s[:start] == goal:
