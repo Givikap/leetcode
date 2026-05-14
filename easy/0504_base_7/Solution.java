1e2ef24e4a2bd4956560391dca3ea8a2c554d33b
Givikap <kaitosekiya@outlook.com>
2026-05-14 03:33:27 -0500

    Add second faster Java solution for '504. Base 7' problem

--- easy/0504_base_7/Solution.java
+++ easy/0504_base_7/Solution.java
@@ -1,28 +1,5 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 class Solution {
   public String convertToBase7(int num) {
-    if (Math.abs(num) < 6) {
-      return Integer.toString(num);
-    }
-
-    final boolean isNegative = num < 0;
-    if (isNegative) {
-      num  = -num;
-    }
-
-    final Deque<String> dq = new ArrayDeque<>();
-
-    while (num != 0) {
-      dq.addFirst(Integer.toString(num % 7));
-      num /= 7;
-    }
-
-    if (isNegative) {
-      dq.addFirst("-");
-    }
-
-    return String.join("", dq);
+    return Integer.toString(num, 7);
   }
 }
