1c36575339e17ab189d6c0f833d4c7d51cddc8a8
Givikap <kaitosekiya@outlook.com>
2026-05-14 02:40:22 -0500

    Add first C++ solution for '504. Base 7' problem

---
+++ easy/0504_base_7/solution.cpp
@@ -0,0 +1,31 @@
+#include <deque>
+#include <string>
+
+class Solution {
+public:
+  std::string convertToBase7(int num) {
+    if (std::abs(num) < 6)
+      return std::to_string(num);
+
+    const bool isNegative = num < 0;
+    if (isNegative)
+      num = -num;
+
+    std::deque<int> dq;
+
+    while (num) {
+      dq.push_front(num % 7);
+      num /= 7;
+    }
+
+    std::string base7Num;
+
+    if (isNegative)
+      base7Num.push_back('-');
+
+    for (const int &digit : dq)
+      base7Num.push_back(48 + digit);
+
+    return base7Num;
+  }
+};
