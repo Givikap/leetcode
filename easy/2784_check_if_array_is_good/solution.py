18897eeffee89dc0f94a67010520aceb8f9e8e26
Givikap <kaitosekiya@outlook.com>
2026-05-16 00:42:32 -0500

    Add second faster Python solution for '2784. Check if Array is Good' problem

--- easy/2784_check_if_array_is_good/solution.py
+++ easy/2784_check_if_array_is_good/solution.py
@@ -1,18 +1,17 @@
-from collections import defaultdict
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        max_num = 0
-        nums_counter = defaultdict(int)
+        nums_counter = Counter(nums)
+        max_num = max(nums_counter)
 
-        for num in nums:
-            max_num = max(max_num, num)
-            nums_counter[num] += 1
+        if nums_counter[max_num] != 2:
+            return False
 
         for num in range(1, max_num):
             if nums_counter[num] != 1:
                 return False
 
-        return nums_counter[max_num] == 2
+        return True
