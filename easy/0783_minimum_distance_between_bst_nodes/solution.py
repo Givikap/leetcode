d0898a50f105100065a51a7abd0b7bd91e069617
Givikap <kaitosekiya@outlook.com>
2026-05-20 01:55:42 -0500

    Add first Python solution for '783. Minimum Distance Between BST Nodes' problem

---
+++ easy/0783_minimum_distance_between_bst_nodes/solution.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from utils.python.nodes import TreeNode
+
+
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        stack = []
+        values = []
+
+        curr = root
+
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+
+            curr = stack.pop()
+            values.append(curr.val)
+            curr = curr.right
+
+        return min(
+            [abs(values[i] - values[i + 1]) for i in range(len(values) - 1)]
+        )
