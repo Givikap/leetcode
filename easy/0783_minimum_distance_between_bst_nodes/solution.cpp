030c9dbd22ee82d5f198968981c4a1a419c22739
Givikap <kaitosekiya@outlook.com>
2026-05-20 02:04:27 -0500

    Add first C++ solution for '783. Minimum Distance Between BST Nodes' problem

---
+++ easy/0783_minimum_distance_between_bst_nodes/solution.cpp
@@ -0,0 +1,32 @@
+#include "../../utils/cpp/nodes.hpp"
+#include <stack>
+#include <vector>
+
+class Solution {
+public:
+  int minDiffInBST(utils::TreeNode *root) {
+    std::stack<utils::TreeNode *> s;
+    std::vector<int> values;
+
+    utils::TreeNode *curr = root;
+
+    while (!s.empty() || curr) {
+      while (curr) {
+        s.push(curr);
+        curr = curr->left;
+      }
+
+      curr = s.top();
+      s.pop();
+
+      values.push_back(curr->val);
+      curr = curr->right;
+    }
+
+    int minDiff = INT32_MAX;
+    for (size_t i = 0; i < values.size() - 1; ++i)
+      minDiff = std::min(minDiff, std::abs(values[i] - values[i + 1]));
+
+    return minDiff;
+  }
+};
