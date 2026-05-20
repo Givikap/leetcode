7b00e33a5c1fb23139631b028fb6b56d392a0f58
Givikap <kaitosekiya@outlook.com>
2026-05-20 02:16:12 -0500

    Add first Java solution for '783. Minimum Distance Between BST Nodes' problem

---
+++ easy/0783_minimum_distance_between_bst_nodes/Solution.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import nodes.TreeNode;
+
+class Solution {
+  public int minDiffInBST(TreeNode root) {
+    final Deque<TreeNode> stack = new ArrayDeque<>();
+    final List<Integer> values = new ArrayList<>();
+
+    TreeNode curr = root;
+
+    while (!stack.isEmpty() || curr != null) {
+      while (curr != null) {
+        stack.push(curr);
+        curr = curr.left;
+      }
+
+      curr = stack.pop();
+
+      values.add(curr.val);
+      curr = curr.right;
+    }
+
+    int minDiff = Integer.MAX_VALUE;
+    for (int i = 0; i < values.size() - 1; ++i) {
+      minDiff = Math.min(minDiff, Math.abs(values.get(i) - values.get(i + 1)));
+    }
+
+    return minDiff;
+  }
+}
