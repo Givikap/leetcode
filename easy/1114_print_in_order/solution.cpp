5c0b55c7fec1a1b423cfc3403f11a67860cb80e7
Givikap <kaitosekiya@outlook.com>
2026-05-25 06:11:48 -0500

    Add second faster C++ solution for '1114. Print in Order' problem

--- easy/1114_print_in_order/solution.cpp
+++ easy/1114_print_in_order/solution.cpp
@@ -1,30 +1,37 @@
-#include <chrono>
 #include <functional>
-#include <thread>
 
 class Foo {
 private:
+  std::condition_variable cv;
+  std::mutex m;
+
   uint state;
 
 public:
   Foo() { state = 0; }
 
   void first(std::function<void()> printFirst) {
-    printFirst();
-    state = 1;
+    {
+      auto lock = std::lock_guard{m};
+      printFirst();
+      state = 1;
+    }
+    cv.notify_all();
   }
 
   void second(std::function<void()> printSecond) {
-    while (state != 1)
-      std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-    printSecond();
-    state = 2;
+    {
+      auto lock = std::unique_lock{m};
+      cv.wait(lock, [&]() { return state == 1; });
+      printSecond();
+      state = 2;
+    }
+    cv.notify_all();
   }
 
   void third(std::function<void()> printThird) {
-    while (state != 2)
-      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    auto lock = std::unique_lock{m};
+    cv.wait(lock, [&]() { return state == 2; });
 
     printThird();
     state = 3;
