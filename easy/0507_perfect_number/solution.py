dc850e3d64944f6a9036daf173b5d29bc3c40f56
Givikap <kaitosekiya@outlook.com>
2026-05-29 03:11:30 -0500

    Add second faster Python solution for '507. Perfect Number' problem

--- easy/0507_perfect_number/solution.py
+++ easy/0507_perfect_number/solution.py
@@ -3,14 +3,13 @@ import math
 
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        if num == 1:
+        if num < 2:
             return False
 
-        divisors = []
+        divisors_sum = 0
 
         for i in range(1, int(math.sqrt(num)) + 1):
             if num % i == 0:
-                divisors.append(i)
-                divisors.append(num // i)
+                divisors_sum += i + num // i
 
-        return sum(divisors) - num == num
+        return divisors_sum - num == num
