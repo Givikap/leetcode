bc1380abb2dc162cdc335b2b6917e555eeea395a
Givikap <kaitosekiya@outlook.com>
2026-06-01 14:04:59 -0500

    Add Node class to C++ utils folder

--- utils/cpp/nodes.hpp
+++ utils/cpp/nodes.hpp
@@ -1,4 +1,16 @@
+#include <vector>
+
 namespace utils {
+
+class Node {
+public:
+  int val;
+  std::vector<Node *> children;
+  Node() {}
+  Node(int _val) : val(_val) {}
+  Node(int _val, std::vector<Node *> _children)
+      : val(_val), children(_children) {}
+};
 
 struct ListNode {
   int val;
