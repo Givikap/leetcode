5694cc96670aac41618897a5d0d01cded84e7ef1
Givikap <kaitosekiya@outlook.com>
2026-06-01 14:00:27 -0500

    Add Node class to Python utils folder

--- utils/python/nodes.py
+++ utils/python/nodes.py
@@ -1,3 +1,16 @@
+from typing import List, Optional
+
+
+class Node:
+    def __init__(
+        self,
+        val: Optional[int] = None,
+        children: Optional[List["Node"]] = None,
+    ):
+        self.val = val
+        self.children = children
+
+
 class ListNode:
     def __init__(self, val: int = 0, next: "ListNode" = None):
         self.val = val
