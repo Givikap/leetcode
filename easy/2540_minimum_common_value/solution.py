a9ba160eb84d7b9f993da4cf5ae6595f8f9b27f7
Givikap <kaitosekiya@outlook.com>
2026-06-04 16:51:03 -0500

    Add second faster Python solution for '2540. Minimum Common Value' problem

--- easy/2540_minimum_common_value/solution.py
+++ easy/2540_minimum_common_value/solution.py
@@ -3,5 +3,21 @@ from typing import List
 
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        nums_set = set(nums1).intersection(set(nums2))
-        return min(nums_set) if nums_set else -1
+        n1 = len(nums1)
+        n2 = len(nums2)
+
+        i1 = 0
+        i2 = 0
+
+        while i1 < n1 and i2 < n2:
+            num1 = nums1[i1]
+            num2 = nums2[i2]
+
+            if num1 == num2:
+                return num1
+            elif num1 < num2:
+                i1 += 1
+            else:
+                i2 += 1
+
+        return -1
