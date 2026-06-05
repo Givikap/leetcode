7d96193a4f0b6f852fe8d7a56fda8a7dbecd7dfb
Givikap <kaitosekiya@outlook.com>
2026-06-05 01:04:34 -0500

    Add third nicer Python solution for '3633. Earliest Finish Time for Land and Water Rides I' problem

--- easy/3633_earliest_finish_time_for_land_and_water_ride_i/solution.py
+++ easy/3633_earliest_finish_time_for_land_and_water_ride_i/solution.py
@@ -11,20 +11,25 @@ class Solution:
     ) -> int:
         earliestFinishTime = float("inf")
 
-        landFt = sum(
+        earliestLandFinishTime = sum(
             min(zip(landStartTime, landDuration), key=lambda t: t[0] + t[1])
         )
-        for waterSt, waterDur in zip(waterStartTime, waterDuration):
-            earliestFinishTime = min(
-                earliestFinishTime, max(landFt, waterSt) + waterDur
-            )
-
-        waterFt = sum(
+        earliestWaterFinishTime = sum(
             min(zip(waterStartTime, waterDuration), key=lambda t: t[0] + t[1])
         )
+
+        earliestFinishTime = float("inf")
+
         for landSt, landDur in zip(landStartTime, landDuration):
             earliestFinishTime = min(
-                earliestFinishTime, max(waterFt, landSt) + landDur
+                earliestFinishTime,
+                max(earliestWaterFinishTime, landSt) + landDur,
+            )
+
+        for waterSt, waterDur in zip(waterStartTime, waterDuration):
+            earliestFinishTime = min(
+                earliestFinishTime,
+                max(earliestLandFinishTime, waterSt) + waterDur,
             )
 
         return earliestFinishTime
