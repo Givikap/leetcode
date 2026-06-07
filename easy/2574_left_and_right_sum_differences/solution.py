2cc4e5fbe43123d9f6ac043fe48a8a0e484be9b3
Givikap <kaitosekiya@outlook.com>
2026-06-06 23:12:20 -0500

    Add third faster Python solution for '2574. Left and Right Sum Differences' problem

--- easy/2574_left_and_right_sum_differences/solution.py
+++ easy/2574_left_and_right_sum_differences/solution.py
@@ -3,16 +3,13 @@ from typing import List
 
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-
         differences = []
 
         numsSum = sum(nums)
         leftSum = 0
 
-        for i in range(n):
-            rightSum = numsSum - leftSum - nums[i]
-            differences.append(abs(leftSum - rightSum))
-            leftSum += nums[i]
+        for num in nums:
+            differences.append(abs(leftSum * 2 + num - numsSum))
+            leftSum += num
 
         return differences
