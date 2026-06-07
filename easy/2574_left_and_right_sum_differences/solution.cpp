9ed89875a1402b3ab278ed311bd4782f7dcdf639
Givikap <kaitosekiya@outlook.com>
2026-06-06 23:17:30 -0500

    Add first C++ solution for '2574. Left and Right Sum Differences' problem

---
+++ easy/2574_left_and_right_sum_differences/solution.cpp
@@ -0,0 +1,19 @@
+#include <numeric>
+#include <vector>
+
+class Solution {
+public:
+  std::vector<int> leftRightDifference(std::vector<int> &nums) {
+    std::vector<int> differences;
+
+    const int numsSum = std::reduce(nums.begin(), nums.end());
+    int leftSum = 0;
+
+    for (const int &num : nums) {
+      differences.push_back(std::abs(leftSum * 2 + num - numsSum));
+      leftSum += num;
+    }
+
+    return differences;
+  }
+};
