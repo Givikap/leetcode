f405b810f006b26a54a3c30e8f080dca4a16657d
Givikap <kaitosekiya@outlook.com>
2026-06-08 17:28:48 -0500

    Add first Java solution for '563. Binary Tree Tilt' problem

---
+++ easy/0563_binary_tree_tilt/Solution.java
@@ -0,0 +1,23 @@
+import nodes.TreeNode;
+
+class Solution {
+  private int tilt;
+
+  public int findTilt(TreeNode root) {
+    this.subtreeSum(root);
+    return this.tilt;
+  }
+
+  private int subtreeSum(TreeNode node) {
+    if (node == null) {
+      return 0;
+    }
+
+    final int leftSum = this.subtreeSum(node.left);
+    final int rightSum = this.subtreeSum(node.right);
+
+    this.tilt += Math.abs(leftSum - rightSum);
+
+    return node.val + leftSum + rightSum;
+  }
+}
