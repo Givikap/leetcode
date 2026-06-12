ca5bc092f16f94ea7acb19b515be94f186f14929
Givikap <kaitosekiya@outlook.com>
2026-06-12 00:59:43 -0500

    Add second faster C++ solution for '589. N-ary Tree Preorder Traversal' problem

--- easy/0589_n_ary_tree_preorder_traversal/solution.cpp
+++ easy/0589_n_ary_tree_preorder_traversal/solution.cpp
@@ -1,5 +1,5 @@
 #include "../../utils/cpp/nodes.hpp"
-#include <deque>
+#include <stack>
 #include <vector>
 
 class Solution {
@@ -9,16 +9,18 @@ public:
       return {};
 
     std::vector<int> vals;
-    std::deque<utils::Node *> dq = {root};
 
-    while (!dq.empty()) {
-      utils::Node *node = dq.front();
-      dq.pop_front();
+    std::stack<utils::Node *> s;
+    s.push(root);
+
+    while (!s.empty()) {
+      utils::Node *node = s.top();
+      s.pop();
 
       vals.push_back(node->val);
 
       for (auto it = node->children.rbegin(); it != node->children.rend(); ++it)
-        dq.push_front(*it);
+        s.push(*it);
     }
 
     return vals;
