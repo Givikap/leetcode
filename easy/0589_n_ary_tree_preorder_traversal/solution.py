b0ca100b6a508c22b8c9444de0e76156d189f95a
Givikap <kaitosekiya@outlook.com>
2026-06-11 23:30:15 -0500

    Add first Python solution for '589. N-ary Tree Preorder Traversal' problem

---
+++ easy/0589_n_ary_tree_preorder_traversal/solution.py
@@ -0,0 +1,23 @@
+from collections import deque
+from typing import List
+
+from utils.python.nodes import Node
+
+
+class Solution:
+    def preorder(self, root: "Node") -> List[int]:
+        if not root:
+            return []
+
+        vals = []
+        dq = deque([root])
+
+        while dq:
+            node = dq.popleft()
+
+            vals.append(node.val)
+
+            for i in range(len(node.children) - 1, -1, -1):
+                dq.appendleft(node.children[i])
+
+        return vals
