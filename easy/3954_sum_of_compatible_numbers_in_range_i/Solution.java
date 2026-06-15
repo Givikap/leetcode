ebf9e881a9e32cefb22f7c25de229544f0b9f90d
Givikap <kaitosekiya@outlook.com>
2026-06-15 03:00:03 -0500

    Add first Java solution for '3954. Sum of Compatible Numbers in Range I' problem

---
+++ easy/3954_sum_of_compatible_numbers_in_range_i/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int sumOfGoodIntegers(int n, int k) {
+    int compatibleNumsSum = 0;
+
+    for (int x = 1; x < n + k + 1; ++x) {
+      if (Math.abs(n - x) <= k && (n & x) == 0) {
+        compatibleNumsSum += x;
+      }
+    }
+
+    return compatibleNumsSum;
+  }
+}
