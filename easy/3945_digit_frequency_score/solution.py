19478aab55b832521fd2d39684914694bcd2b560
Givikap <kaitosekiya@outlook.com>
2026-06-16 12:54:59 -0500

    Add second Python solution for '3945. Digit Frequency Score' problem

--- easy/3945_digit_frequency_score/solution.py
+++ easy/3945_digit_frequency_score/solution.py
@@ -1,14 +1,9 @@
 class Solution:
     def digitFrequencyScore(self, n: int) -> int:
-        frequencies = [0] * 10
-
-        while n:
-            frequencies[n % 10] += 1
-            n //= 10
-
         score = 0
 
-        for digit, frequency in enumerate(frequencies):
-            score += digit * frequency
+        while n:
+            score += n % 10
+            n //= 10
 
         return score
