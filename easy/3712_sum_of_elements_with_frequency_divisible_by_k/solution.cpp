0d59184ddf17d1fd2f9a2b96118cb04a6adc2af2
Givikap <kaitosekiya@outlook.com>
2026-06-17 02:51:44 -0500

    Add third faster C++ solution for '3712. Sum of Elements With Frequency Divisible by K' problem

--- easy/3712_sum_of_elements_with_frequency_divisible_by_k/solution.cpp
+++ easy/3712_sum_of_elements_with_frequency_divisible_by_k/solution.cpp
@@ -5,15 +5,15 @@
 class Solution {
 public:
   int sumDivisibleByK(std::vector<int> &nums, int k) {
-    std::unordered_map<int, int> numsCounter;
+    int numsCounter[101];
     for (const int &num : nums)
       ++numsCounter[num];
 
     int numsSum = 0;
 
-    for (const auto &[num, count] : numsCounter) {
-      if (count % k == 0)
-        numsSum += num * count;
+    for (size_t i = 0; i < 101; ++i) {
+      if (numsCounter[i] % k == 0)
+        numsSum += i * numsCounter[i];
     }
 
     return numsSum;
