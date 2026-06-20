c917eaaef05cf881703294f395590680a7094e29
Givikap <kaitosekiya@outlook.com>
2026-06-20 13:15:12 -0500

    Add second faster Python solution for '697. Degree of an Array' problem

--- easy/0697_degree_of_an_array/solution.py
+++ easy/0697_degree_of_an_array/solution.py
@@ -4,17 +4,19 @@ from typing import List
 
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        reversedNums = list(reversed(nums))
-
         numsCounter = Counter(nums)
 
         maxCount = max(numsCounter.values())
-        minLen = len(nums)
+        if maxCount == 1:
+            return 1
+
+        numsLen = len(nums)
+        minLen = numsLen
 
         for num, count in numsCounter.items():
             if count == maxCount:
                 left = nums.index(num)
-                right = len(nums) - reversedNums.index(num) - 1
+                right = numsLen - nums[::-1].index(num) - 1
 
                 minLen = min(minLen, right - left + 1)
 
