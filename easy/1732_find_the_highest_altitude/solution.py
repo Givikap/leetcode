539f786c983483bc639315bc75df9fd12a2983d4
Givikap <kaitosekiya@outlook.com>
2026-06-21 00:21:48 -0500

    Add first Python solution for '1732. Find the Highest Altitude' problem

---
+++ easy/1732_find_the_highest_altitude/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        maxAltitude = 0
+        currAltitude = 0
+
+        for change in gain:
+            currAltitude += change
+            maxAltitude = max(maxAltitude, currAltitude)
+
+        return maxAltitude
