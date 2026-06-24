9930cf999ed547dc49d1cc6b97c42df456f7af9f
Givikap <kaitosekiya@outlook.com>
2026-06-24 17:52:59 -0500

    Add second faster C++ solution for '1331. Rank Transform of an Array' problem

--- easy/1331_rank_transform_of_an_array/solution.cpp
+++ easy/1331_rank_transform_of_an_array/solution.cpp
@@ -1,15 +1,19 @@
-#include <set>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
 public:
   std::vector<int> arrayRankTransform(std::vector<int> &arr) {
-    std::set<int> arrSet(arr.begin(), arr.end());
+    std::vector<int> arrCopy = arr;
+    std::sort(arrCopy.begin(), arrCopy.end());
+
     std::unordered_map<int, int> rankMap;
 
     int rank = 0;
-    for (const int &num : arrSet)
-      rankMap[num] = ++rank;
+    for (const int &num : arrCopy) {
+      if (rankMap.find(num) == rankMap.end())
+        rankMap[num] = ++rank;
+    }
 
     for (size_t i = 0; i < arr.size(); ++i)
       arr[i] = rankMap[arr[i]];
