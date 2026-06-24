fc277bb6642c9c5477a97837e194cebc4864ebd4
Givikap <kaitosekiya@outlook.com>
2026-06-24 17:49:32 -0500

    Add second faster Java solution for '1331. Rank Transform of an Array' problem

--- easy/1331_rank_transform_of_an_array/Solution.java
+++ easy/1331_rank_transform_of_an_array/Solution.java
@@ -1,21 +1,20 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 class Solution {
   public int[] arrayRankTransform(int[] arr) {
-    final SortedSet<Integer> arrSet = new TreeSet<>();
-    for (final int num : arr) {
-      arrSet.add(num);
-    }
+    final int[] arrCopy = arr.clone();
+    Arrays.sort(arrCopy);
 
     final Map<Integer, Integer> rankMap = new HashMap<>();
 
-    int rank = 0;
-    for (final int num : arrSet) {
-      rankMap.put(num, rank);
-      ++rank;
+    int rank = 1;
+    for (final int num : arrCopy) {
+      if (!rankMap.containsKey(num)) {
+        rankMap.put(num, rank);
+        ++rank;
+      }
     }
 
     for (int i = 0; i < arr.length; ++i) {
