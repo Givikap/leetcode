9f073a93078ad837f7b63efa059cb037b5c1d0da
Givikap <kaitosekiya@outlook.com>
2026-06-27 10:41:00 -0500

    Add second faster Python solution for '1833. Maximum Ice Cream Bars' problem

--- medium/1833_maximum_ice_cream_bars/solution.py
+++ medium/1833_maximum_ice_cream_bars/solution.py
@@ -3,13 +3,24 @@ from typing import List
 
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costsCounter = [0] * (max(costs) + 1)
+        for cost in costs:
+            costsCounter[cost] += 1
+
         iceCreamCount = 0
 
-        for cost in sorted(costs):
-            if cost > coins:
-                break
+        for cost in range(1, len(costsCounter)):
+            count = costsCounter[cost]
+            if not count:
+                continue
 
-            coins -= cost
-            iceCreamCount += 1
+            iceCreamsCost = cost * count
+            if coins >= iceCreamsCost:
+                coins -= iceCreamsCost
+                iceCreamCount += count
+            else:
+                while coins >= cost:
+                    coins -= cost
+                    iceCreamCount += 1
 
         return iceCreamCount
