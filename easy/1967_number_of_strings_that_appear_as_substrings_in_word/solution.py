a52824e8c91c5e3dc2c52cabf8e6b0bd89729d9f
Givikap <kaitosekiya@outlook.com>
2026-06-29 02:34:07 -0500

    Add second faster Python solution for '1967. Number of Strings That Appear as Substrings in Word' problem

--- easy/1967_number_of_strings_that_appear_as_substrings_in_word/solution.py
+++ easy/1967_number_of_strings_that_appear_as_substrings_in_word/solution.py
@@ -3,10 +3,4 @@ from typing import List
 
 class Solution:
     def numOfStrings(self, patterns: List[str], word: str) -> int:
-        stringsCount = 0
-
-        for pattern in patterns:
-            if pattern in word:
-                stringsCount += 1
-
-        return stringsCount
+        return sum(1 for pattern in patterns if pattern in word)
