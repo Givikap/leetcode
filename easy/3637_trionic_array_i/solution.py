3b3bc397c9eb29d031bac265128d6af5cb06a8d3
Givikap <kaitosekiya@outlook.com>
2026-06-30 00:38:16 -0500

    Add second nicer Python solution for '3637. Trionic Array I' problem

--- easy/3637_trionic_array_i/solution.py
+++ easy/3637_trionic_array_i/solution.py
@@ -5,24 +5,20 @@ class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
         numsLen = len(nums)
 
-        p = 0
-        while p < numsLen - 1 and nums[p] < nums[p + 1]:
-            p += 1
+        i = 0
+        while i < numsLen - 1 and nums[i] < nums[i + 1]:
+            i += 1
 
-        if p == 0 or p == numsLen - 1:
+        if i == 0 or i == numsLen - 1:
             return False
 
-        q = p
-        while q < numsLen - 1 and nums[q] > nums[q + 1]:
-            q += 1
+        while i < numsLen - 1 and nums[i] > nums[i + 1]:
+            i += 1
 
-        if q == numsLen - 1:
+        if i == numsLen - 1:
             return False
 
-        while q < numsLen - 1:
-            if nums[q] >= nums[q + 1]:
-                return False
+        while i < numsLen - 1 and nums[i] < nums[i + 1]:
+            i += 1
 
-            q += 1
-
-        return q == numsLen - 1
+        return i == numsLen - 1
