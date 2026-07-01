c74f0229a9817fc656516fe1d0fdc7c15b74d4d3
Givikap <kaitosekiya@outlook.com>
2026-07-01 00:55:27 -0500

    Add second faster Python solution for '1358. Number of Substrings Containing All Three Characters' problem

--- medium/1358_number_of_substrings_containing_all_three_characters/solution.py
+++ medium/1358_number_of_substrings_containing_all_three_characters/solution.py
@@ -1,28 +1,10 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        sLen = len(s)
-
-        abcCounter = [0] * 3
-        right = 0
-
+        lastAbcMap = [-1] * 3
         substringsCount = 0
 
-        for left in range(sLen - 2):
-            while right < sLen:
-                abcCounter[ord(s[right]) - 97] += 1
-                right += 1
-
-                if abcCounter[0] and abcCounter[1] and abcCounter[2]:
-                    break
-
-            if not (abcCounter[0] and abcCounter[1] and abcCounter[2]):
-                break
-
-            substringsCount += sLen - right + 1
-
-            abcCounter[ord(s[left]) - 97] -= 1
-            while abcCounter[0] and abcCounter[1] and abcCounter[2]:
-                right -= 1
-                abcCounter[ord(s[right]) - 97] -= 1
+        for i, c in enumerate(s):
+            lastAbcMap[ord(c) - 97] = i
+            substringsCount += min(lastAbcMap) + 1
 
         return substringsCount
