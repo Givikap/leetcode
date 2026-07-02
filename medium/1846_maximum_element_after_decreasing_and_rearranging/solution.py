028e3b43e33de3d46323a3d730acd2d61ab4b78a
Givikap <kaitosekiya@outlook.com>
2026-07-02 00:28:24 -0500

    Add second faster Python solution for '1846. Maximum Element After Decreasing and Rearranging' problem

--- medium/1846_maximum_element_after_decreasing_and_rearranging/solution.py
+++ medium/1846_maximum_element_after_decreasing_and_rearranging/solution.py
@@ -9,7 +9,6 @@ class Solution:
         arr[0] = 1
 
         for i in range(1, len(arr)):
-            if arr[i] - arr[i - 1] > 1:
-                arr[i] = arr[i - 1] + 1
+            arr[i] = min(arr[i], arr[i - 1] + 1)
 
         return arr[-1]
