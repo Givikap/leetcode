3df58c6edd369e22d1589890ae517021f7515798
Givikap <kaitosekiya@outlook.com>
2026-07-03 01:57:58 -0500

    Add second faster C++ solution for '643. Maximum Average Subarray I' problem

--- easy/0643_maximum_average_subarray_i/solution.cpp
+++ easy/0643_maximum_average_subarray_i/solution.cpp
@@ -4,11 +4,14 @@
 class Solution {
 public:
   double findMaxAverage(std::vector<int> &nums, int k) {
-    int currSum = std::reduce(nums.begin(), nums.begin() + k);
+    int currSum = 0;
+    for (size_t i = 0; i < static_cast<size_t>(k); ++i)
+      currSum += nums[i];
+
     int maxSum = currSum;
 
-    for (size_t i = k; i < nums.size(); ++i) {
-      currSum = currSum + nums[i] - nums[i - k];
+    for (size_t i = static_cast<size_t>(k); i < nums.size(); ++i) {
+      currSum += nums[i] - nums[i - static_cast<size_t>(k)];
       maxSum = std::max(maxSum, currSum);
     }
 
