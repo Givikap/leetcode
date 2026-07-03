884bb5612eb20050b1aaffe7a7775558d0db76fe
Givikap <kaitosekiya@outlook.com>
2026-07-02 20:11:28 -0500

    Add second faster Java solution for '1846. Maximum Element After Decreasing and Rearranging' problem

--- medium/1846_maximum_element_after_decreasing_and_rearranging/Solution.java
+++ medium/1846_maximum_element_after_decreasing_and_rearranging/Solution.java
@@ -1,14 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
   public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-    Arrays.sort(arr);
-    arr[0] = 1;
+    final int n = arr.length;
+    final int[] numsCounter = new int[n];
 
-    for (int i = 1; i < arr.length; ++i) {
-      arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+    for (final int num : arr) {
+      ++numsCounter[Math.min(num, n) - 1];
     }
 
-    return arr[arr.length - 1];
+    int maxElement = 1;
+    for (int num = 2; num <= n; num++) {
+      maxElement = Math.min(maxElement + numsCounter[num - 1], num);
+    }
+
+    return maxElement;
   }
 }
