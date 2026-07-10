52d469706acf9d5991db9fd62e5e0c79837a714a
Givikap <kaitosekiya@outlook.com>
2026-07-09 19:19:58 -0500

    Add second faster C++ solution for '787. Cheapest Flights Within K Stops' problem

--- medium/0787_cheapest_flights_within_k_stops/solution.cpp
+++ medium/0787_cheapest_flights_within_k_stops/solution.cpp
@@ -1,57 +1,35 @@
-#include <unordered_map>
-#include <unordered_set>
+#include <queue>
 #include <vector>
 
 class Solution {
 public:
   int findCheapestPrice(int n, std::vector<std::vector<int>> &flights, int src,
                         int dst, int k) {
-    std::unordered_map<int, std::vector<std::pair<int, int>>> flightsMap;
+    std::vector<std::vector<std::pair<int, int>>> flightsMap(n);
     for (const std::vector<int> &flight : flights)
       flightsMap[flight[0]].push_back({flight[1], flight[2]});
 
-    if (flightsMap.find(src) == flightsMap.end())
-      return -1;
+    std::queue<std::tuple<int, int, int>> q;
+    q.push({0, src, 0});
 
-    std::vector<int> froms = {src};
-    std::unordered_set<int> visited;
-    std::unordered_map<int, int> pricesMap = {{src, 0}};
+    std::vector<int> pricesMap(n, INT32_MAX);
+    pricesMap[src] = 0;
 
-    for (int _ = 0; _ < k; ++_) {
-      std::vector<int> tos;
-      visited.clear();
-      auto prevPricesMap = pricesMap;
+    while (!q.empty()) {
+      auto [stops, from, price] = q.front();
+      q.pop();
 
-      for (const int &from : froms) {
-        if (visited.find(from) != visited.end())
-          continue;
+      if (stops > k)
+        continue;
 
-        for (const auto &[to, price] : flightsMap[from]) {
-          if (pricesMap.find(to) != pricesMap.end())
-            pricesMap[to] =
-                std::min(pricesMap[to], prevPricesMap[from] + price);
-          else
-            pricesMap[to] = prevPricesMap[from] + price;
-
-          tos.push_back(to);
-        }
-
-        visited.insert(from);
-      }
-      froms = tos;
-    }
-
-    for (const int &from : froms) {
-      for (const auto &[to, price] : flightsMap[from]) {
-        if (to == dst) {
-          if (pricesMap.find(to) != pricesMap.end())
-            pricesMap[to] = std::min(pricesMap[to], pricesMap[from] + price);
-          else
-            pricesMap[to] = pricesMap[from] + price;
+      for (const auto &[to, prc] : flightsMap[from]) {
+        if (pricesMap[to] > price + prc) {
+          pricesMap[to] = price + prc;
+          q.push({stops + 1, to, pricesMap[to]});
         }
       }
     }
 
-    return pricesMap.find(dst) != pricesMap.end() ? pricesMap[dst] : -1;
+    return pricesMap[dst] != INT32_MAX ? pricesMap[dst] : -1;
   }
 };
