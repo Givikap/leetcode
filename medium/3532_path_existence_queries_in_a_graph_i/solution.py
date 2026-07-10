b2e5091a97297010bf5ea22b5d4f03b91ef1ce34
Givikap <kaitosekiya@outlook.com>
2026-07-10 15:21:28 -0500

    Add second faster Python solution for '3532. Path Existence Queries in a Graph I' problem

--- medium/3532_path_existence_queries_in_a_graph_i/solution.py
+++ medium/3532_path_existence_queries_in_a_graph_i/solution.py
@@ -1,16 +1,17 @@
 from typing import List
 
-from utils.python import UnionFind
-
 
 class Solution:
     def pathExistenceQueries(
         self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]
     ) -> List[bool]:
-        uf = UnionFind(n)
+        groupsMap = [0] * n
+        currGroup = 0
 
         for i in range(1, n):
-            if nums[i] - nums[i - 1] <= maxDiff:
-                uf.union(i, i - 1)
+            if nums[i] - nums[i - 1] > maxDiff:
+                currGroup += 1
 
-        return [uf.find(i) == uf.find(j) for i, j in queries]
+            groupsMap[i] = currGroup
+
+        return [groupsMap[i] == groupsMap[j] for i, j in queries]
