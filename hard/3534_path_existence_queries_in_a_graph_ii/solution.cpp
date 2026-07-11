d37dcdf025cd11a947e8c22745e2f2f4389afc36
Givikap <kaitosekiya@outlook.com>
2026-07-10 23:35:11 -0500

    Add second faster C++ solution for '3534. Path Existence Queries in a Graph II' problem

--- hard/3534_path_existence_queries_in_a_graph_ii/solution.cpp
+++ hard/3534_path_existence_queries_in_a_graph_ii/solution.cpp
@@ -8,58 +8,53 @@ public:
   std::vector<int>
   pathExistenceQueries(int n, std::vector<int> &nums, int maxDiff,
                        std::vector<std::vector<int>> &queries) {
-    std::vector<size_t> sortedIndices(n);
-    std::iota(sortedIndices.begin(), sortedIndices.end(), 0);
-    std::sort(sortedIndices.begin(), sortedIndices.end(),
-              [&](int a, int b) { return nums[a] < nums[b]; });
-
-    std::vector<int> sortedVals(n);
-    for (size_t i = 0; i < n; i++)
-      sortedVals[i] = nums[sortedIndices[i]];
+    std::vector<std::pair<int, size_t>> items(n);
+    for (size_t i = 0; i < n; ++i)
+      items[i] = {nums[i], i};
+    std::sort(items.begin(), items.end());
 
     std::vector<size_t> indicesMap(n);
-    for (size_t i = 0; i < n; i++)
-      indicesMap[sortedIndices[i]] = i;
+    for (size_t i = 0; i < n; ++i)
+      indicesMap[items[i].second] = i;
 
-    size_t k = (size_t)log2(n) + 1;
+    int jumps[100001][18];
 
-    std::vector<std::vector<int>> jumps(n, std::vector<int>(k, n - 1));
+    size_t i = 0, j;
 
-    for (size_t i = 0; i < n; i++) {
-      auto it = upper_bound(sortedVals.begin(), sortedVals.end(),
-                            sortedVals[i] + maxDiff);
-      jumps[i][0] = (int)(it - sortedVals.begin()) - 1;
-    }
+    for (j = 0; j < n; ++j)
+      for (; items[j].first - items[i].first > maxDiff; ++i)
+        jumps[i][0] = j - 1;
 
-    for (size_t j = 1; j < k; j++)
-      for (size_t i = 0; i < n; i++)
+    for (; i < n; ++i)
+      jumps[i][0] = n - 1;
+
+    for (j = 1; j < 18; ++j)
+      for (i = 0; i < n; ++i)
         jumps[i][j] = jumps[jumps[i][j - 1]][j - 1];
 
-    std::vector<int> pathsExist;
+    std::vector<int> pathsExist(queries.size(), -1);
 
-    for (const auto &query : queries) {
-      int u = query[0], v = query[1];
+    for (i = 0; i < queries.size(); ++i) {
+      size_t left = indicesMap[queries[i][0]];
+      size_t right = indicesMap[queries[i][1]];
 
-      if (u == v) {
-        pathsExist.push_back(0);
+      if (left == right) {
+        pathsExist[i] = 0;
       } else {
-        size_t start = indicesMap[u], end = indicesMap[v];
-        if (start > end)
-          std::swap(start, end);
+        if (left > right)
+          std::swap(left, right);
 
         int hops = 0;
 
-        for (int j = k - 1; j >= 0; j--) {
-          if (jumps[start][j] < end) {
-            start = jumps[start][j];
+        for (j = 17; j < -1; --j) {
+          if (jumps[left][j] < right) {
+            left = jumps[left][j];
             hops += (1 << j);
           }
         }
 
-        if (jumps[start][0] >= end)
-          pathsExist.push_back(hops + 1);
-        else
-          pathsExist.push_back(-1);
+        if (jumps[left][0] >= right)
+          pathsExist[i] = hops + 1;
       }
     }
 
