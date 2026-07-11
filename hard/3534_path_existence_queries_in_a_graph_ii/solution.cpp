e302304c3414dc38fe8b2209c7bbeebf5825aba6
Givikap <kaitosekiya@outlook.com>
2026-07-10 23:43:11 -0500

    Add third faster C++ solution for '3534. Path Existence Queries in a Graph II' problem

--- hard/3534_path_existence_queries_in_a_graph_ii/solution.cpp
+++ hard/3534_path_existence_queries_in_a_graph_ii/solution.cpp
@@ -8,19 +8,29 @@ public:
   std::vector<int>
   pathExistenceQueries(int n, std::vector<int> &nums, int maxDiff,
                        std::vector<std::vector<int>> &queries) {
+    size_t i, j;
+
     std::vector<std::pair<int, size_t>> items(n);
-    for (size_t i = 0; i < n; ++i)
+    for (i = 0; i < n; ++i)
       items[i] = {nums[i], i};
     std::sort(items.begin(), items.end());
 
     std::vector<size_t> indicesMap(n);
-    for (size_t i = 0; i < n; ++i)
+    for (i = 0; i < n; ++i)
       indicesMap[items[i].second] = i;
+
+    std::vector<int> reachable(n, -1);
+
+    for (i = n - 2; i < -1; --i) {
+      if (items[i + 1].first - items[i].first <= maxDiff)
+        reachable[i] = reachable[i + 1];
+      else
+        reachable[i] = i;
+    }
 
     int jumps[100001][18];
 
-    size_t i = 0, j;
-
+    i = 0;
     for (j = 0; j < n; ++j)
       for (; items[j].first - items[i].first > maxDiff; ++i)
         jumps[i][0] = j - 1;
@@ -32,29 +42,30 @@ public:
       for (i = 0; i < n; ++i)
         jumps[i][j] = jumps[jumps[i][j - 1]][j - 1];
 
-    std::vector<int> pathsExist(queries.size(), -1);
+    std::vector<int> pathsExist(queries.size());
 
     for (i = 0; i < queries.size(); ++i) {
-      size_t left = indicesMap[queries[i][0]];
-      size_t right = indicesMap[queries[i][1]];
+      size_t u = queries[i][0];
+      size_t v = queries[i][1];
 
-      if (left == right) {
-        pathsExist[i] = 0;
-      } else {
+      if (u != v) {
+        size_t left = indicesMap[u];
+        size_t right = indicesMap[v];
         if (left > right)
           std::swap(left, right);
 
-        int hops = 0;
+        if (reachable[left] < right)
+          pathsExist[i] = -1;
+        else {
+          pathsExist[i] = 1;
 
-        for (j = 17; j < -1; --j) {
-          if (jumps[left][j] < right) {
-            left = jumps[left][j];
-            hops += (1 << j);
+          for (j = 17; j < -1; --j) {
+            if (jumps[left][j] < right) {
+              left = jumps[left][j];
+              pathsExist[i] += (1 << j);
+            }
           }
         }
-
-        if (jumps[left][0] >= right)
-          pathsExist[i] = hops + 1;
       }
     }
 
