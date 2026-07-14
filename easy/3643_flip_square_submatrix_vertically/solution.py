81a60ccfd02a8a617b47cfdf652f30bfff47b9da
Givikap <kaitosekiya@outlook.com>
2026-07-14 01:04:50 -0500

    Add second faster Python solution for '3643. Flip Square Submatrix Vertically' problem

--- easy/3643_flip_square_submatrix_vertically/solution.py
+++ easy/3643_flip_square_submatrix_vertically/solution.py
@@ -5,13 +5,17 @@ class Solution:
     def reverseSubmatrix(
         self, grid: List[List[int]], x: int, y: int, k: int
     ) -> List[List[int]]:
-        for topRow, bottomRow in zip(
-            range(x, x + k // 2), range(x + k - 1, -1, -1)
-        ):
+        topRow = x
+        bottomRow = x + k - 1
+
+        while topRow < bottomRow:
             for col in range(y, y + k):
                 grid[topRow][col], grid[bottomRow][col] = (
                     grid[bottomRow][col],
                     grid[topRow][col],
                 )
+
+            topRow += 1
+            bottomRow -= 1
 
         return grid
