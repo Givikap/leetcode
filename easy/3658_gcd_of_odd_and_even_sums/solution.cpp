70c3e74e0e3b3a5cfa404212936429fd0be891db
Givikap <kaitosekiya@outlook.com>
2026-07-15 00:43:40 -0500

    Add second faster C++ solution for '3658. GCD of Odd and Even Sums' problem

--- easy/3658_gcd_of_odd_and_even_sums/solution.cpp
+++ easy/3658_gcd_of_odd_and_even_sums/solution.cpp
@@ -2,15 +2,5 @@
 
 class Solution {
 public:
-  int gcdOfOddEvenSums(int n) {
-    int sumOdd = 0;
-    for (int num = 1; num < n * 2; num += 2)
-      sumOdd += num;
-
-    int sumEven = 0;
-    for (int num = 2; num <= n * 2; num += 2)
-      sumEven += num;
-
-    return std::gcd(sumOdd, sumEven);
-  }
+  int gcdOfOddEvenSums(int n) { return n; }
 };
