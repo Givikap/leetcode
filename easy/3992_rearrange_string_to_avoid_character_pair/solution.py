4f4e828194e0505f136fb28f6343bb8f1241c1e3
Givikap <kaitosekiya@outlook.com>
2026-07-18 14:00:42 -0500

    Add second faster Python solution for '3992. Rearrange String to Avoid Character Pair' problem

--- easy/3992_rearrange_string_to_avoid_character_pair/solution.py
+++ easy/3992_rearrange_string_to_avoid_character_pair/solution.py
@@ -1,19 +1,9 @@
-from collections import Counter
-
-
 class Solution:
     def rearrangeString(self, s: str, x: str, y: str) -> str:
-        sCounter = Counter(s)
+        t = [y] * s.count(y)
 
-        t = []
-
-        for _ in range(sCounter.get(y, 0)):
-            t.append(y)
-
-        sCounter.pop(y, 0)
-
-        for c, count in sCounter.items():
-            for _ in range(count):
+        for c in s:
+            if c != y:
                 t.append(c)
 
         return "".join(t)
