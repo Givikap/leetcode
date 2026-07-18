3d54bb235cb2fb241b6450244979556fd335dfaa
Givikap <kaitosekiya@outlook.com>
2026-07-18 16:55:52 -0500

    Add second faster Python solution for '3978. Unique Middle Element' problem

--- easy/3978_unique_middle_element/solution.py
+++ easy/3978_unique_middle_element/solution.py
@@ -1,15 +1,3 @@
 class Solution:
     def isMiddleElementUnique(self, nums: list[int]) -> bool:
-        left = 0
-        right = len(nums) - 1
-
-        midNum = nums[len(nums) // 2]
-
-        while left < right:
-            if nums[left] == midNum or nums[right] == midNum:
-                return False
-
-            left += 1
-            right -= 1
-
-        return True
+        return nums.count(nums[len(nums) // 2]) == 1
