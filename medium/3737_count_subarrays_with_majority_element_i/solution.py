768d1c32d63b86aa159fa7a016a77b8d501db15c
Givikap <kaitosekiya@outlook.com>
2026-07-18 13:20:23 -0500

    Add second faster Python solution for '3737. Count Subarrays With Majority Element I' problem

--- medium/3737_count_subarrays_with_majority_element_i/solution.py
+++ medium/3737_count_subarrays_with_majority_element_i/solution.py
@@ -5,16 +5,18 @@ class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
         n = len(nums)
 
-        prefixCounts = [0] * (len(nums) + 1)
-
-        for i in range(1, n + 1):
-            prefixCounts[i] = prefixCounts[i - 1] + (nums[i - 1] == target)
-
         count = 0
 
         for i in range(n):
+            targetCount = 0
+
             for j in range(i, n):
-                if 2 * (prefixCounts[j + 1] - prefixCounts[i]) > j - i + 1:
+                if nums[j] == target:
+                    targetCount += 1
+                else:
+                    targetCount -= 1
+
+                if targetCount > 0:
                     count += 1
 
         return count
