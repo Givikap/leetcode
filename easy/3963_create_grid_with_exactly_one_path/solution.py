da349c4d209fec0b96436b6e5a21561511c20227
Givikap <kaitosekiya@outlook.com>
2026-07-18 17:17:14 -0500

    Add second faster Python solution for '3963. Create Grid With Exactly One Path' problem

--- easy/3963_create_grid_with_exactly_one_path/solution.py
+++ easy/3963_create_grid_with_exactly_one_path/solution.py
@@ -3,9 +3,7 @@ from typing import List
 
 class Solution:
     def createGrid(self, m: int, n: int) -> List[str]:
-        grid = ["." * n]
-
-        for _ in range(m - 1):
-            grid.append(("#" * (n - 1)) + ".")
+        grid = ["#" * (n - 1) + "." for _ in range(m)]
+        grid[0] = "." * n
 
         return grid
