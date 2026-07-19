4c4c1cea182e18e874a9fe0f4bfac7a609523a75
Givikap <kaitosekiya@outlook.com>
2026-07-19 01:25:50 -0500

    Add second faster Python solution for '3936. Minimum Swaps to Move Zeros to End' problem

--- easy/3936_minimum_swaps_to_move_zeros_to_end/solution.py
+++ easy/3936_minimum_swaps_to_move_zeros_to_end/solution.py
@@ -3,24 +3,4 @@ from typing import List
 
 class Solution:
     def minimumSwaps(self, nums: List[int]) -> int:
-        left = 0
-        right = len(nums) - 1
-
-        swaps = 0
-
-        while True:
-            while left < right and nums[left] != 0:
-                left += 1
-
-            while left < right and nums[right] == 0:
-                right -= 1
-
-            if left < right:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-                right -= 1
-                swaps += 1
-            else:
-                break
-
-        return swaps
+        return nums[: -nums.count(0)].count(0)
