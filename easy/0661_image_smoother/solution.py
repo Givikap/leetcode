793c375669c5635fc3fe98e6c11c8376b26e749d
Givikap <kaitosekiya@outlook.com>
2026-07-22 21:02:09 -0500

    Add second faster Python solution for '661. Image Smoother' problem

--- easy/0661_image_smoother/solution.py
+++ easy/0661_image_smoother/solution.py
@@ -6,31 +6,18 @@ class Solution:
         rows = len(img)
         cols = len(img[0])
 
-        offsets = [
-            (-1, -1),
-            (0, -1),
-            (1, -1),
-            (-1, 0),
-            (0, 0),
-            (1, 0),
-            (-1, 1),
-            (0, 1),
-            (1, 1),
-        ]
-
         imgSmoothed = [[0] * cols for _ in range(rows)]
 
         for row in range(rows):
             for col in range(cols):
-                cells = []
+                cellsSum = 0
+                cellsCount = 0
 
-                for rowOffset, colOffset in offsets:
-                    if (
-                        0 <= row + rowOffset < rows
-                        and 0 <= col + colOffset < cols
-                    ):
-                        cells.append(img[row + rowOffset][col + colOffset])
+                for r in range(max(0, row - 1), min(rows, row + 2)):
+                    for c in range(max(0, col - 1), min(cols, col + 2)):
+                        cellsSum += img[r][c]
+                        cellsCount += 1
 
-                imgSmoothed[row][col] = sum(cells) // len(cells)
+                imgSmoothed[row][col] = cellsSum // cellsCount
 
         return imgSmoothed
